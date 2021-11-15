b0c99135f23b58462a1016a887566570adfc088f
Bryan Bernhart <bryan.bernhart@intel.com>
2021-11-15 10:04:41 -0800

    Improve MemoryPool.h class docstring.

--- src/MemoryPool.h
+++ src/MemoryPool.h
@@ -23,7 +23,7 @@ namespace gpgmm {
 
     class MemoryAllocation;
 
-    // Stores memory blocks or heaps.
+    // Stores a collection of memory blocks or heaps.
     class MemoryPool {
       public:
         virtual ~MemoryPool() = default;
