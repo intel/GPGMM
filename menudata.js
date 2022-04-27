70c100c096f4130999aec46faf7611b15496f233
bbernhar <bbernhar@users.noreply.github.com>
2022-04-27 20:01:38 +0000

    deploy: f4f8e0bf73c39b93e25e442a8bc580eda8f83281

--- menudata.js
+++ menudata.js
@@ -149,7 +149,6 @@ var menudata={children:[
 {text:"All",url:"globals.html",children:[
 {text:"a",url:"globals.html#index_a"},
 {text:"d",url:"globals.html#index_d"},
-{text:"e",url:"globals.html#index_e"},
 {text:"g",url:"globals.html#index_g"},
 {text:"h",url:"globals.html#index_h"},
 {text:"i",url:"globals.html#index_i"},
@@ -160,12 +159,12 @@ var menudata={children:[
 {text:"t",url:"globals.html#index_t"},
 {text:"u",url:"globals.html#index_u"}]},
 {text:"Functions",url:"globals_func.html",children:[
-{text:"e",url:"globals_func.html#index_e"},
 {text:"g",url:"globals_func.html#index_g"},
 {text:"h",url:"globals_func.html#index_h"},
 {text:"i",url:"globals_func.html#index_i"},
 {text:"l",url:"globals_func.html#index_l"},
 {text:"m",url:"globals_func.html#index_m"},
+{text:"r",url:"globals_func.html#index_r"},
 {text:"t",url:"globals_func.html#index_t"}]},
 {text:"Variables",url:"globals_vars.html"},
 {text:"Enumerations",url:"globals_enum.html"},
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,15 +1,14 @@
 var searchData=
 [
   ['empty_0',['empty',['../classgpgmm_1_1_linked_list.html#ad448483bcf5a201c0b6f86fad43ade63',1,'gpgmm::LinkedList']]],
-  ['enablememoryleakdetection_1',['EnableMemoryLeakDetection',['../_g_p_g_m_m_test_8cpp.html#add53a11d041bf6a3e07f0f205e06dafb',1,'GPGMMTest.cpp']]],
-  ['end_2',['end',['../classgpgmm_1_1_linked_list.html#a534b4d4ca230230333206839b0822528',1,'gpgmm::LinkedList::end()'],['../classgpgmm_1_1d3d12_1_1_residency_set.html#ab468071c2752d9f82d9e08c73910f7ba',1,'gpgmm::d3d12::ResidencySet::end()'],['../classgpgmm_1_1_memory_cache.html#a11332292a65154e33968db6f41e3cbed',1,'gpgmm::MemoryCache::end()']]],
-  ['endelapsedtime_3',['EndElapsedTime',['../classgpgmm_1_1_platform_time.html#abeb9c84a8f7c52149cb2f4c60b06033e',1,'gpgmm::PlatformTime::EndElapsedTime()'],['../classgpgmm_1_1_windows_time.html#a43efe1821064c19969745130f691201f',1,'gpgmm::WindowsTime::EndElapsedTime()']]],
-  ['enqueuetraceevent_4',['EnqueueTraceEvent',['../classgpgmm_1_1_event_trace_writer.html#abe5129b0b3f506ef7944dccfb9d370e0',1,'gpgmm::EventTraceWriter']]],
-  ['errorevent_5',['ErrorEvent',['../namespacegpgmm.html#a9ce85bdbbdfb1efebb93a6eea5f3db43',1,'gpgmm']]],
-  ['errorlog_6',['ErrorLog',['../namespacegpgmm.html#aebed7617bb2d08b0a2eac27f84cac24e',1,'gpgmm']]],
-  ['event_7',['Event',['../classgpgmm_1_1_event.html#afbb9549815e21e5ad23d97b2d20ce668',1,'gpgmm::Event']]],
-  ['eventmessage_8',['EventMessage',['../classgpgmm_1_1_event_message.html#a0eac75cefa628cde14026b8f8368ded5',1,'gpgmm::EventMessage::EventMessage(const LogSeverity &amp;level, const char *name, int messageId=0)'],['../classgpgmm_1_1_event_message.html#aee3e8d8f190d8ea04a72cb42c0c27d0b',1,'gpgmm::EventMessage::EventMessage(EventMessage &amp;&amp;other)=default']]],
-  ['eventtracewriter_9',['EventTraceWriter',['../classgpgmm_1_1_event_trace_writer.html#a5b34f87f224ac1ae0c21cdb00504499c',1,'gpgmm::EventTraceWriter']]],
-  ['evict_10',['Evict',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#a5dc4f4b070eb246d079e6701b7ab6cc4',1,'gpgmm::d3d12::ResidencyManager']]],
-  ['executecommandlists_11',['ExecuteCommandLists',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#ab889d80e34d4ebca85727887ac42f982',1,'gpgmm::d3d12::ResidencyManager']]]
+  ['end_1',['end',['../classgpgmm_1_1_linked_list.html#a534b4d4ca230230333206839b0822528',1,'gpgmm::LinkedList::end()'],['../classgpgmm_1_1d3d12_1_1_residency_set.html#ab468071c2752d9f82d9e08c73910f7ba',1,'gpgmm::d3d12::ResidencySet::end()'],['../classgpgmm_1_1_memory_cache.html#a11332292a65154e33968db6f41e3cbed',1,'gpgmm::MemoryCache::end()']]],
+  ['endelapsedtime_2',['EndElapsedTime',['../classgpgmm_1_1_platform_time.html#abeb9c84a8f7c52149cb2f4c60b06033e',1,'gpgmm::PlatformTime::EndElapsedTime()'],['../classgpgmm_1_1_windows_time.html#a43efe1821064c19969745130f691201f',1,'gpgmm::WindowsTime::EndElapsedTime()']]],
+  ['enqueuetraceevent_3',['EnqueueTraceEvent',['../classgpgmm_1_1_event_trace_writer.html#abe5129b0b3f506ef7944dccfb9d370e0',1,'gpgmm::EventTraceWriter']]],
+  ['errorevent_4',['ErrorEvent',['../namespacegpgmm.html#a9ce85bdbbdfb1efebb93a6eea5f3db43',1,'gpgmm']]],
+  ['errorlog_5',['ErrorLog',['../namespacegpgmm.html#aebed7617bb2d08b0a2eac27f84cac24e',1,'gpgmm']]],
+  ['event_6',['Event',['../classgpgmm_1_1_event.html#afbb9549815e21e5ad23d97b2d20ce668',1,'gpgmm::Event']]],
+  ['eventmessage_7',['EventMessage',['../classgpgmm_1_1_event_message.html#a0eac75cefa628cde14026b8f8368ded5',1,'gpgmm::EventMessage::EventMessage(const LogSeverity &amp;level, const char *name, int messageId=0)'],['../classgpgmm_1_1_event_message.html#aee3e8d8f190d8ea04a72cb42c0c27d0b',1,'gpgmm::EventMessage::EventMessage(EventMessage &amp;&amp;other)=default']]],
+  ['eventtracewriter_8',['EventTraceWriter',['../classgpgmm_1_1_event_trace_writer.html#a5b34f87f224ac1ae0c21cdb00504499c',1,'gpgmm::EventTraceWriter']]],
+  ['evict_9',['Evict',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#a5dc4f4b070eb246d079e6701b7ab6cc4',1,'gpgmm::d3d12::ResidencyManager']]],
+  ['executecommandlists_10',['ExecuteCommandLists',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#ab889d80e34d4ebca85727887ac42f982',1,'gpgmm::d3d12::ResidencyManager']]]
 ];
