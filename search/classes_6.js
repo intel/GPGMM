b08bae5ace401c13a583203deee6aa97197651b6
bbernhar <bbernhar@users.noreply.github.com>
2022-05-03 18:17:40 +0000

    deploy: ddf9255b737c9ff9263e4330dadc8ff32aec80ce

--- search/classes_6.js
+++ search/classes_6.js
@@ -4,7 +4,9 @@ var searchData=
   ['gpgmmcapturereplaytestenvironment_1',['GPGMMCaptureReplayTestEnvironment',['../class_g_p_g_m_m_capture_replay_test_environment.html',1,'']]],
   ['gpgmmtestbase_2',['GPGMMTestBase',['../class_g_p_g_m_m_test_base.html',1,'']]],
   ['gpgmmtestenvironment_3',['GPGMMTestEnvironment',['../class_g_p_g_m_m_test_environment.html',1,'']]],
-  ['gpresourceallocation_5ft_4',['GpResourceAllocation_T',['../structgpgmm_1_1vk_1_1_gp_resource_allocation___t.html',1,'gpgmm::vk']]],
-  ['gpresourceallocationcreateinfo_5',['GpResourceAllocationCreateInfo',['../structgpgmm_1_1vk_1_1_gp_resource_allocation_create_info.html',1,'gpgmm::vk']]],
-  ['gpresourceallocator_5ft_6',['GpResourceAllocator_T',['../structgpgmm_1_1vk_1_1_gp_resource_allocator___t.html',1,'gpgmm::vk']]]
+  ['gpresourceallocation_4',['GpResourceAllocation',['../struct_gp_resource_allocation.html',1,'']]],
+  ['gpresourceallocation_5ft_5',['GpResourceAllocation_T',['../structgpgmm_1_1vk_1_1_gp_resource_allocation___t.html',1,'gpgmm::vk']]],
+  ['gpresourceallocationcreateinfo_6',['GpResourceAllocationCreateInfo',['../structgpgmm_1_1vk_1_1_gp_resource_allocation_create_info.html',1,'gpgmm::vk']]],
+  ['gpresourceallocator_7',['GpResourceAllocator',['../struct_gp_resource_allocator.html',1,'']]],
+  ['gpresourceallocator_5ft_8',['GpResourceAllocator_T',['../structgpgmm_1_1vk_1_1_gp_resource_allocator___t.html',1,'gpgmm::vk']]]
 ];
