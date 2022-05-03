e1914be68ae36b2fe0f716ea62341acf871640d2
bbernhar <bbernhar@users.noreply.github.com>
2022-05-03 22:42:23 +0000

    deploy: 891ae2658ea35ffe42f88cf351972b1acd1fa5b3

--- search/functions_1.js
+++ search/functions_1.js
@@ -3,5 +3,5 @@ var searchData=
   ['begin_0',['begin',['../classgpgmm_1_1_memory_cache.html#a83b2bb64c4228a1f91b8fa1542af0a8e',1,'gpgmm::MemoryCache::begin()'],['../classgpgmm_1_1d3d12_1_1_residency_set.html#afcddba2f1c3f4432e5ac63ae8b6f1c8a',1,'gpgmm::d3d12::ResidencySet::begin()']]],
   ['buddyblockallocator_1',['BuddyBlockAllocator',['../classgpgmm_1_1_buddy_block_allocator.html#a7546c244311f2f227599279125ba3da1',1,'gpgmm::BuddyBlockAllocator']]],
   ['buddymemoryallocator_2',['BuddyMemoryAllocator',['../classgpgmm_1_1_buddy_memory_allocator.html#ad422be1ee9807d4e8a9831b832445516',1,'gpgmm::BuddyMemoryAllocator']]],
-  ['bufferallocator_3',['BufferAllocator',['../classgpgmm_1_1d3d12_1_1_buffer_allocator.html#a8408d710774e6aa669afd36eb01e38b7',1,'gpgmm::d3d12::BufferAllocator']]]
+  ['bufferallocator_3',['BufferAllocator',['../classgpgmm_1_1d3d12_1_1_buffer_allocator.html#a8c34f9fa9f3eec264564a3ccf77d2dd9',1,'gpgmm::d3d12::BufferAllocator']]]
 ];
