35570b0ad0619ffdf2c6f2d2d27f404974de6129
bbernhar <bbernhar@users.noreply.github.com>
2022-05-05 18:23:43 +0000

    deploy: 7182a14f300c75790755e29f25b6166054c522dd

--- search/all_7.js
+++ search/all_7.js
@@ -1,13 +1,14 @@
 var searchData=
 [
   ['handleassertionfailure_0',['HandleAssertionFailure',['../_assert_8cpp.html#aef7607be3d8c191d54a749d55e9f2a67',1,'HandleAssertionFailure(const char *file, const char *function, int line, const char *condition):&#160;Assert.cpp'],['../_assert_8h.html#aef7607be3d8c191d54a749d55e9f2a67',1,'HandleAssertionFailure(const char *file, const char *function, int line, const char *condition):&#160;Assert.cpp']]],
-  ['hasoneref_1',['HasOneRef',['../classgpgmm_1_1_ref_counted.html#a37f79c230eb0f82f20a8fe455f7664b8',1,'gpgmm::RefCounted']]],
-  ['head_2',['head',['../classgpgmm_1_1_linked_list.html#a37254c01e28b0154513bb15d7b5ef923',1,'gpgmm::LinkedList']]],
-  ['heap_3',['Heap',['../classgpgmm_1_1d3d12_1_1_heap.html',1,'gpgmm::d3d12::Heap'],['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a38bd84c06fec78de5d0a7d2b572f1820',1,'gpgmm::d3d12::HEAP_INFO::Heap()'],['../classgpgmm_1_1d3d12_1_1_heap.html#a952b1d52bf868c4f0faa0c59ad3b96ed',1,'gpgmm::d3d12::Heap::Heap()']]],
-  ['heap_5fdesc_4',['HEAP_DESC',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html',1,'gpgmm::d3d12']]],
-  ['heap_5finfo_5',['HEAP_INFO',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html',1,'gpgmm::d3d12']]],
-  ['heapd3d12_2ecpp_6',['HeapD3D12.cpp',['../_heap_d3_d12_8cpp.html',1,'']]],
-  ['heapd3d12_2eh_7',['HeapD3D12.h',['../_heap_d3_d12_8h.html',1,'']]],
-  ['heapoffset_8',['HeapOffset',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a0160d7cd2425e8e144e4924c19abd517',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO']]],
-  ['heaptype_9',['HeapType',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aa720936c4215d487a3e391e144836691',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
+  ['haschild_1',['HasChild',['../classgpgmm_1_1_allocator_node.html#a8a4dafc2e1ff152659d845ea1a6932f0',1,'gpgmm::AllocatorNode::HasChild()'],['../class_test_memory_allocator.html#a9a31e440da4650dbb0dd049298f745b9',1,'TestMemoryAllocator::HasChild()']]],
+  ['hasoneref_2',['HasOneRef',['../classgpgmm_1_1_ref_counted.html#a37f79c230eb0f82f20a8fe455f7664b8',1,'gpgmm::RefCounted']]],
+  ['head_3',['head',['../classgpgmm_1_1_linked_list.html#a37254c01e28b0154513bb15d7b5ef923',1,'gpgmm::LinkedList']]],
+  ['heap_4',['Heap',['../classgpgmm_1_1d3d12_1_1_heap.html',1,'gpgmm::d3d12::Heap'],['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a38bd84c06fec78de5d0a7d2b572f1820',1,'gpgmm::d3d12::HEAP_INFO::Heap()'],['../classgpgmm_1_1d3d12_1_1_heap.html#a952b1d52bf868c4f0faa0c59ad3b96ed',1,'gpgmm::d3d12::Heap::Heap()']]],
+  ['heap_5fdesc_5',['HEAP_DESC',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html',1,'gpgmm::d3d12']]],
+  ['heap_5finfo_6',['HEAP_INFO',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html',1,'gpgmm::d3d12']]],
+  ['heapd3d12_2ecpp_7',['HeapD3D12.cpp',['../_heap_d3_d12_8cpp.html',1,'']]],
+  ['heapd3d12_2eh_8',['HeapD3D12.h',['../_heap_d3_d12_8h.html',1,'']]],
+  ['heapoffset_9',['HeapOffset',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a0160d7cd2425e8e144e4924c19abd517',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO']]],
+  ['heaptype_10',['HeapType',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aa720936c4215d487a3e391e144836691',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
 ];
