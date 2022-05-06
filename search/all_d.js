d7cba290d9c4d33c2c2005fa453e25686a03c24e
bbernhar <bbernhar@users.noreply.github.com>
2022-05-06 23:32:48 +0000

    deploy: 64bb165dad3f74859ab0de7a3516a2e52e723b3d

--- search/all_d.js
+++ search/all_d.js
@@ -1,11 +1,10 @@
 var searchData=
 [
   ['name_0',['name',['../struct_trace_file.html#a86e9dc6e842f180b7a7b6e3ad5f4bd2c',1,'TraceFile']]],
-  ['neverallocate_1',['NeverAllocate',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a7d7b04df4ac858359500cccb4795981a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
-  ['next_2',['next',['../classgpgmm_1_1_link_node.html#af130013a33a0163a6dcf4515825a7ab8',1,'gpgmm::LinkNode']]],
-  ['nextpoweroftwo_3',['NextPowerOfTwo',['../namespacegpgmm.html#a7105ddb398cb2ce2e5b8c5823cb12ec7',1,'gpgmm']]],
-  ['noncopyable_4',['NonCopyable',['../classgpgmm_1_1_non_copyable.html',1,'gpgmm::NonCopyable'],['../classgpgmm_1_1_non_copyable.html#a588c58b8330a3d62608141c07a4132bf',1,'gpgmm::NonCopyable::NonCopyable()=default'],['../classgpgmm_1_1_non_copyable.html#a87e9480f0d92752ea967c83b79c064b8',1,'gpgmm::NonCopyable::NonCopyable(NonCopyable &amp;&amp;)=default']]],
-  ['noncopyable_2eh_5',['NonCopyable.h',['../_non_copyable_8h.html',1,'']]],
-  ['numofhits_6',['NumOfHits',['../structgpgmm_1_1_cache_stats.html#a81aa18f541e3b8cc43806234a969b689',1,'gpgmm::CacheStats']]],
-  ['numofmisses_7',['NumOfMisses',['../structgpgmm_1_1_cache_stats.html#a9331f38d59d151a56f6a70542d2c4a93',1,'gpgmm::CacheStats']]]
+  ['next_1',['next',['../classgpgmm_1_1_link_node.html#af130013a33a0163a6dcf4515825a7ab8',1,'gpgmm::LinkNode']]],
+  ['nextpoweroftwo_2',['NextPowerOfTwo',['../namespacegpgmm.html#a7105ddb398cb2ce2e5b8c5823cb12ec7',1,'gpgmm']]],
+  ['noncopyable_3',['NonCopyable',['../classgpgmm_1_1_non_copyable.html',1,'gpgmm::NonCopyable'],['../classgpgmm_1_1_non_copyable.html#a588c58b8330a3d62608141c07a4132bf',1,'gpgmm::NonCopyable::NonCopyable()=default'],['../classgpgmm_1_1_non_copyable.html#a87e9480f0d92752ea967c83b79c064b8',1,'gpgmm::NonCopyable::NonCopyable(NonCopyable &amp;&amp;)=default']]],
+  ['noncopyable_2eh_4',['NonCopyable.h',['../_non_copyable_8h.html',1,'']]],
+  ['numofhits_5',['NumOfHits',['../structgpgmm_1_1_cache_stats.html#a81aa18f541e3b8cc43806234a969b689',1,'gpgmm::CacheStats']]],
+  ['numofmisses_6',['NumOfMisses',['../structgpgmm_1_1_cache_stats.html#a9331f38d59d151a56f6a70542d2c4a93',1,'gpgmm::CacheStats']]]
 ];
