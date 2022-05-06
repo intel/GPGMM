a9024b7edeab5e1f79b5f5abefd77aa685b31e4d
bbernhar <bbernhar@users.noreply.github.com>
2022-05-06 21:01:47 +0000

    deploy: 6a95b10c718e1a6c0b511ca0cc5108feff50efbc

--- search/defines_1.js
+++ search/defines_1.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['d3d12_5ffeature_5fd3d12_5foptions7_0',['D3D12_FEATURE_D3D12_OPTIONS7',['../d3d12__platform_8h.html#a49f81b8fa2e9f1f0e733d25f640b2288',1,'d3d12_platform.h']]],
-  ['d3d12_5ffeature_5fdata_5fd3d12_5foptions7_1',['D3D12_FEATURE_DATA_D3D12_OPTIONS7',['../d3d12__platform_8h.html#a4d403d47f246fb6fdb59eb5554bab5a5',1,'d3d12_platform.h']]],
-  ['d3d12_5fheap_5fflag_5fcreate_5fnot_5fresident_2',['D3D12_HEAP_FLAG_CREATE_NOT_RESIDENT',['../d3d12__platform_8h.html#ab8ef82481f8a0488a9dfcccd703594ac',1,'d3d12_platform.h']]],
-  ['define_5foperators_5ffor_5fflags_3',['DEFINE_OPERATORS_FOR_FLAGS',['../_flags_8h.html#a54054e7fb2195df4be3a3bee7f07bb34',1,'Flags.h']]]
+  ['define_5foperators_5ffor_5fflags_0',['DEFINE_OPERATORS_FOR_FLAGS',['../_flags_8h.html#a54054e7fb2195df4be3a3bee7f07bb34',1,'Flags.h']]]
 ];
