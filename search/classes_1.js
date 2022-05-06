84dedec3a673f76824424f27f8cd814c712c36c3
bbernhar <bbernhar@users.noreply.github.com>
2022-05-06 23:11:31 +0000

    deploy: c273ea97571608a2196a11420ab5b6ed33351350

--- search/classes_1.js
+++ search/classes_1.js
@@ -4,5 +4,6 @@ var searchData=
   ['blockallocator_1',['BlockAllocator',['../classgpgmm_1_1_block_allocator.html',1,'gpgmm']]],
   ['buddyblockallocator_2',['BuddyBlockAllocator',['../classgpgmm_1_1_buddy_block_allocator.html',1,'gpgmm']]],
   ['buddymemoryallocator_3',['BuddyMemoryAllocator',['../classgpgmm_1_1_buddy_memory_allocator.html',1,'gpgmm']]],
-  ['bufferallocator_4',['BufferAllocator',['../classgpgmm_1_1d3d12_1_1_buffer_allocator.html',1,'gpgmm::d3d12']]]
+  ['buddymemoryallocatortests_4',['BuddyMemoryAllocatorTests',['../class_buddy_memory_allocator_tests.html',1,'']]],
+  ['bufferallocator_5',['BufferAllocator',['../classgpgmm_1_1d3d12_1_1_buffer_allocator.html',1,'gpgmm::d3d12']]]
 ];
--- search/variables_0.js
+++ search/variables_0.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['adapter_0',['Adapter',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#af36130c1c5c13528871e33b57d13911c',1,'gpgmm::d3d12::RESIDENCY_DESC::Adapter()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a017d4dcb6c2df114409f28b4e701de78',1,'gpgmm::d3d12::ALLOCATOR_DESC::Adapter()']]],
-  ['allocatememory_1',['AllocateMemory',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a8f18e9843ad3e5f3e1d0344b1858e70c',1,'gpgmm::vk::VulkanFunctions']]],
-  ['allocationdescriptor_2',['allocationDescriptor',['../structgpgmm_1_1d3d12_1_1_c_r_e_a_t_e___r_e_s_o_u_r_c_e___d_e_s_c.html#a4f6838340fa519320a572273fe5aa120',1,'gpgmm::d3d12::CREATE_RESOURCE_DESC']]],
-  ['allocatorprofile_3',['AllocatorProfile',['../struct_test_enviroment_params.html#ab0555104413aaf29072dad44bfa569c1',1,'TestEnviromentParams']]]
+  ['alignment_1',['Alignment',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a421aa93aa5d3fe42580be07e37d13987',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
+  ['allocatememory_2',['AllocateMemory',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a8f18e9843ad3e5f3e1d0344b1858e70c',1,'gpgmm::vk::VulkanFunctions']]],
+  ['allocationdescriptor_3',['allocationDescriptor',['../structgpgmm_1_1d3d12_1_1_c_r_e_a_t_e___r_e_s_o_u_r_c_e___d_e_s_c.html#a4f6838340fa519320a572273fe5aa120',1,'gpgmm::d3d12::CREATE_RESOURCE_DESC']]],
+  ['allocatorprofile_4',['AllocatorProfile',['../struct_test_enviroment_params.html#ab0555104413aaf29072dad44bfa569c1',1,'TestEnviromentParams']]],
+  ['alwaysprefetch_5',['AlwaysPrefetch',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#afd8863eaf9267b64008d7e02010ef4cd',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]]
 ];
