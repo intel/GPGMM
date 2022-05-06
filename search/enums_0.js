a8b04cca48880d1fcefdb1e6bc984637528556c4
bbernhar <bbernhar@users.noreply.github.com>
2022-05-06 16:44:44 +0000

    deploy: c3f879fc778ae413ba171045df70d3bec8cfa3d7

--- search/enums_0.js
+++ search/enums_0.js
@@ -4,8 +4,7 @@ var searchData=
   ['allocationmethod_1',['AllocationMethod',['../namespacegpgmm.html#a73f15de0f698ad7e9a69df91d6da1d8d',1,'gpgmm']]],
   ['allocator_5fflags_2',['ALLOCATOR_FLAGS',['../namespacegpgmm_1_1d3d12.html#a41e5f9a1d561e4bc68c1e2965f8860b8',1,'gpgmm::d3d12']]],
   ['allocator_5fmessage_5fid_3',['ALLOCATOR_MESSAGE_ID',['../namespacegpgmm.html#a9d813b66f6a9ec8f8331919c20bafb79',1,'gpgmm']]],
-  ['allocator_5fmessage_5fseverity_4',['ALLOCATOR_MESSAGE_SEVERITY',['../namespacegpgmm_1_1d3d12.html#ae2b91bc7a6d3e8e3ad36d6cc02ca598a',1,'gpgmm::d3d12']]],
-  ['allocator_5frecord_5fflags_5',['ALLOCATOR_RECORD_FLAGS',['../namespacegpgmm_1_1d3d12.html#a2e37df42bebf066b8cec7189b305682c',1,'gpgmm::d3d12']]],
-  ['allocator_5frecord_5fscope_6',['ALLOCATOR_RECORD_SCOPE',['../namespacegpgmm_1_1d3d12.html#af588c6de3c0eb44ef046610a1caee5a8',1,'gpgmm::d3d12']]],
-  ['allocatorprofile_7',['AllocatorProfile',['../_g_p_g_m_m_capture_replay_tests_8h.html#a72bdf48239b01f0790bbbdedf60a6a52',1,'GPGMMCaptureReplayTests.h']]]
+  ['allocator_5frecord_5fflags_4',['ALLOCATOR_RECORD_FLAGS',['../namespacegpgmm_1_1d3d12.html#a2e37df42bebf066b8cec7189b305682c',1,'gpgmm::d3d12']]],
+  ['allocator_5frecord_5fscope_5',['ALLOCATOR_RECORD_SCOPE',['../namespacegpgmm_1_1d3d12.html#af588c6de3c0eb44ef046610a1caee5a8',1,'gpgmm::d3d12']]],
+  ['allocatorprofile_6',['AllocatorProfile',['../_g_p_g_m_m_capture_replay_tests_8h.html#a72bdf48239b01f0790bbbdedf60a6a52',1,'GPGMMCaptureReplayTests.h']]]
 ];
