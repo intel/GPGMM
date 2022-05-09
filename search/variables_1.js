5ea306481510837e5c7015570782630641d0e29b
bbernhar <bbernhar@users.noreply.github.com>
2022-05-09 19:43:00 +0000

    deploy: c5183daf40de53b65fa9c172d4665e28d2ea8ccb

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['bindbuffermemory_0',['BindBufferMemory',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a24ad152a285a2ba1bd28eed880f745de',1,'gpgmm::vk::VulkanFunctions']]],
-  ['budget_1',['Budget',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#aac67b1916a22771093bc77268102f36b',1,'gpgmm::d3d12::RESIDENCY_DESC::Budget()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a81ccf1803c45d2c3419b114e20b096cc',1,'gpgmm::d3d12::ALLOCATOR_DESC::Budget()']]]
+  ['budget_1',['Budget',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#aac67b1916a22771093bc77268102f36b',1,'gpgmm::d3d12::RESIDENCY_DESC']]]
 ];
