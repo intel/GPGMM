99584f55a13155b4d4b595fb98641398b6f983ea
bbernhar <bbernhar@users.noreply.github.com>
2022-05-09 23:38:02 +0000

    deploy: 1745f4b212df20b027adf807911c048215eee529

--- search/defines_1.js
+++ search/defines_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['define_5foperators_5ffor_5fflags_0',['DEFINE_OPERATORS_FOR_FLAGS',['../_flags_8h.html#a54054e7fb2195df4be3a3bee7f07bb34',1,'Flags.h']]]
+  ['d3d12_5fheap_5fflag_5fcreate_5fnot_5fresident_0',['D3D12_HEAP_FLAG_CREATE_NOT_RESIDENT',['../d3d12__platform_8h.html#ab8ef82481f8a0488a9dfcccd703594ac',1,'d3d12_platform.h']]],
+  ['define_5foperators_5ffor_5fflags_1',['DEFINE_OPERATORS_FOR_FLAGS',['../_flags_8h.html#a54054e7fb2195df4be3a3bee7f07bb34',1,'Flags.h']]]
 ];
