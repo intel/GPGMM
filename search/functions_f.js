229e2d1a31169bb6abc336a046d015bc431720f0
bbernhar <bbernhar@users.noreply.github.com>
2022-05-10 03:07:15 +0000

    deploy: a4dc0257a961a33ad09a45e467523edf2c264d8b

--- search/functions_f.js
+++ search/functions_f.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['queryinterface_0',['QueryInterface',['../classgpgmm_1_1d3d12_1_1_i_unknown_impl.html#af148920b9a182a494b17e9d940a4539a',1,'gpgmm::d3d12::IUnknownImpl']]]
+  ['queryinterface_0',['QueryInterface',['../classgpgmm_1_1d3d12_1_1_i_unknown_impl.html#af148920b9a182a494b17e9d940a4539a',1,'gpgmm::d3d12::IUnknownImpl']]],
+  ['queryvideomemoryinfo_1',['QueryVideoMemoryInfo',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#a31376e92d2182d553c1252ac38d55f4e',1,'gpgmm::d3d12::ResidencyManager']]]
 ];
