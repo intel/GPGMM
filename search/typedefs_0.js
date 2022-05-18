fa1ac9c67787f8c0ecd21431d08dda3ae845b4e8
bbernhar <bbernhar@users.noreply.github.com>
2022-05-18 22:51:20 +0000

    deploy: 0e2428a712603ebdb74598252e0385ed88b429bd

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -4,5 +4,6 @@ var searchData=
   ['allocationtype_1',['AllocationType',['../structgpgmm_1_1d3d12_1_1_backend_trait.html#a22ef15c9938bc38eec2be09bda2ea342',1,'gpgmm::d3d12::BackendTrait']]],
   ['allocator_5fflags_5ftype_2',['ALLOCATOR_FLAGS_TYPE',['../namespacegpgmm_1_1d3d12.html#addd8571a3cf9c1c8893f16f664612090',1,'gpgmm::d3d12']]],
   ['allocator_5frecord_5fflags_5ftype_3',['ALLOCATOR_RECORD_FLAGS_TYPE',['../namespacegpgmm_1_1d3d12.html#a8ab05c28f34f8023a5de8a2037daa4e5',1,'gpgmm::d3d12']]],
-  ['allocator_5frecord_5fscope_5ftype_4',['ALLOCATOR_RECORD_SCOPE_TYPE',['../namespacegpgmm_1_1d3d12.html#ab52ecb8f9358cd8afc4a5a30cfb9d93f',1,'gpgmm::d3d12']]]
+  ['allocator_5frecord_5fscope_5ftype_4',['ALLOCATOR_RECORD_SCOPE_TYPE',['../namespacegpgmm_1_1d3d12.html#ab52ecb8f9358cd8afc4a5a30cfb9d93f',1,'gpgmm::d3d12']]],
+  ['allocator_5fsuballocation_5falgorithm_5ftype_5',['ALLOCATOR_SUBALLOCATION_ALGORITHM_TYPE',['../namespacegpgmm_1_1d3d12.html#a51f5c1dba97e19b3a65d333249f12fa2',1,'gpgmm::d3d12']]]
 ];
