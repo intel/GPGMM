eb6cf4f1e4368f26fb336d3265e67c9adbbfd9f7
bbernhar <bbernhar@users.noreply.github.com>
2022-05-21 00:56:54 +0000

    deploy: c7b3e8652e61a12f23de9e206ce34679dcbc713a

--- search/classes_d.js
+++ search/classes_d.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['platformtime_0',['PlatformTime',['../classgpgmm_1_1_platform_time.html',1,'gpgmm']]],
   ['pool_5finfo_1',['POOL_INFO',['../structgpgmm_1_1_p_o_o_l___i_n_f_o.html',1,'gpgmm']]],
-  ['pooledmemoryallocator_2',['PooledMemoryAllocator',['../classgpgmm_1_1_pooled_memory_allocator.html',1,'gpgmm']]],
-  ['pooledmemoryallocatortests_3',['PooledMemoryAllocatorTests',['../class_pooled_memory_allocator_tests.html',1,'']]]
+  ['pooledmemoryallocator_2',['PooledMemoryAllocator',['../classgpgmm_1_1_pooled_memory_allocator.html',1,'gpgmm']]]
 ];
