55588673bcf921e4837e4a0ef137ed1d62f64653
bbernhar <bbernhar@users.noreply.github.com>
2022-05-23 20:18:41 +0000

    deploy: 6b8b33e820f72256d861248706dc9703181b6083

--- search/variables_0.js
+++ search/variables_0.js
@@ -7,6 +7,7 @@ var searchData=
   ['allocationdescriptor_4',['allocationDescriptor',['../structgpgmm_1_1d3d12_1_1_c_r_e_a_t_e___r_e_s_o_u_r_c_e___d_e_s_c.html#a4f6838340fa519320a572273fe5aa120',1,'gpgmm::d3d12::CREATE_RESOURCE_DESC']]],
   ['allocator_5',['Allocator',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a72f7eadef3065427b5cfac78359f7fb2',1,'gpgmm::MEMORY_ALLOCATION_INFO']]],
   ['allocatorprofile_6',['AllocatorProfile',['../struct_test_enviroment_params.html#ab0555104413aaf29072dad44bfa569c1',1,'TestEnviromentParams']]],
-  ['alwaysprefetch_7',['AlwaysPrefetch',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#afd8863eaf9267b64008d7e02010ef4cd',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
-  ['availableforallocation_8',['AvailableForAllocation',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a26e2dcc129782a2a4a7c6e5c97ff230a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]]
+  ['alwayscachesize_7',['AlwaysCacheSize',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#af94c06358013c63f0f142db8b69ce567',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
+  ['alwaysprefetch_8',['AlwaysPrefetch',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#afd8863eaf9267b64008d7e02010ef4cd',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
+  ['availableforallocation_9',['AvailableForAllocation',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a26e2dcc129782a2a4a7c6e5c97ff230a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]]
 ];
