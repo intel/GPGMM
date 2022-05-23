14922486752c7dcc13aa2e41cfceda0fe10f0fdc
bbernhar <bbernhar@users.noreply.github.com>
2022-05-23 19:11:02 +0000

    deploy: f55128750031187936519fe2bea2aecff26b58e4

--- search/variables_2.js
+++ search/variables_2.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['clearvalue_0',['clearValue',['../structgpgmm_1_1d3d12_1_1_c_r_e_a_t_e___r_e_s_o_u_r_c_e___d_e_s_c.html#ac7152d41c2fd3eecb9af66ae034b1d47',1,'gpgmm::d3d12::CREATE_RESOURCE_DESC']]],
-  ['createbuffer_1',['CreateBuffer',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a2d447560b50885fa0c6f1e62ec9d920d',1,'gpgmm::vk::VulkanFunctions']]],
-  ['createimage_2',['CreateImage',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#accd6e6be635aa1a7b447a4316bd046bd',1,'gpgmm::vk::VulkanFunctions']]],
-  ['currentusage_3',['CurrentUsage',['../struct_capture_replay_memory_stats.html#a667a9d17b86cccf51b5df9bba2f8e682',1,'CaptureReplayMemoryStats']]]
+  ['cachesize_0',['CacheSize',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a509462c671682c534748ed79b094cd1a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
+  ['clearvalue_1',['clearValue',['../structgpgmm_1_1d3d12_1_1_c_r_e_a_t_e___r_e_s_o_u_r_c_e___d_e_s_c.html#ac7152d41c2fd3eecb9af66ae034b1d47',1,'gpgmm::d3d12::CREATE_RESOURCE_DESC']]],
+  ['createbuffer_2',['CreateBuffer',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a2d447560b50885fa0c6f1e62ec9d920d',1,'gpgmm::vk::VulkanFunctions']]],
+  ['createimage_3',['CreateImage',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#accd6e6be635aa1a7b447a4316bd046bd',1,'gpgmm::vk::VulkanFunctions']]],
+  ['currentusage_4',['CurrentUsage',['../struct_capture_replay_memory_stats.html#a667a9d17b86cccf51b5df9bba2f8e682',1,'CaptureReplayMemoryStats']]]
 ];
