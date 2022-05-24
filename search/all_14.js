53b11a3e4049e3624d48098f73d01624e8839eb3
bbernhar <bbernhar@users.noreply.github.com>
2022-05-24 22:46:42 +0000

    deploy: c7543ed103c431989256bb90da3066d0ed5c75c0

--- search/all_14.js
+++ search/all_14.js
@@ -8,12 +8,11 @@ var searchData=
   ['updateresidency_5',['UpdateResidency',['../classgpgmm_1_1d3d12_1_1_resource_allocation.html#a49f91f6c880a59b5ad7875a2a075c2cd',1,'gpgmm::d3d12::ResourceAllocation']]],
   ['usedblockcount_6',['UsedBlockCount',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_o_r___i_n_f_o.html#a2b1d4c70eaed581ddc5b2baea6be8bd8',1,'gpgmm::MEMORY_ALLOCATOR_INFO']]],
   ['usedblockusage_7',['UsedBlockUsage',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_o_r___i_n_f_o.html#a3e933937a80dbfb2f0463c8344bbf881',1,'gpgmm::MEMORY_ALLOCATOR_INFO']]],
-  ['usedetailedtimingevents_8',['UseDetailedTimingEvents',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___r_e_c_o_r_d___o_p_t_i_o_n_s.html#a8407f808d092f3e62ba522900fbbe62a',1,'gpgmm::d3d12::ALLOCATOR_RECORD_OPTIONS']]],
-  ['usedmemorycount_9',['UsedMemoryCount',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_o_r___i_n_f_o.html#a9281555c6bf6473fbb649a74c21f7364',1,'gpgmm::MEMORY_ALLOCATOR_INFO']]],
-  ['usedmemoryusage_10',['UsedMemoryUsage',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_o_r___i_n_f_o.html#af396fc166fc5002fab9a5e5dd7e7a1c9',1,'gpgmm::MEMORY_ALLOCATOR_INFO']]],
-  ['utils_2ecpp_11',['Utils.cpp',['../_utils_8cpp.html',1,'']]],
-  ['utils_2eh_12',['Utils.h',['../_utils_8h.html',1,'']]],
-  ['utilsd3d12_2ecpp_13',['UtilsD3D12.cpp',['../_utils_d3_d12_8cpp.html',1,'']]],
-  ['utilsd3d12_2eh_14',['UtilsD3D12.h',['../_utils_d3_d12_8h.html',1,'']]],
-  ['utilstest_2ecpp_15',['UtilsTest.cpp',['../_utils_test_8cpp.html',1,'']]]
+  ['usedmemorycount_8',['UsedMemoryCount',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_o_r___i_n_f_o.html#a9281555c6bf6473fbb649a74c21f7364',1,'gpgmm::MEMORY_ALLOCATOR_INFO']]],
+  ['usedmemoryusage_9',['UsedMemoryUsage',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_o_r___i_n_f_o.html#af396fc166fc5002fab9a5e5dd7e7a1c9',1,'gpgmm::MEMORY_ALLOCATOR_INFO']]],
+  ['utils_2ecpp_10',['Utils.cpp',['../_utils_8cpp.html',1,'']]],
+  ['utils_2eh_11',['Utils.h',['../_utils_8h.html',1,'']]],
+  ['utilsd3d12_2ecpp_12',['UtilsD3D12.cpp',['../_utils_d3_d12_8cpp.html',1,'']]],
+  ['utilsd3d12_2eh_13',['UtilsD3D12.h',['../_utils_d3_d12_8h.html',1,'']]],
+  ['utilstest_2ecpp_14',['UtilsTest.cpp',['../_utils_test_8cpp.html',1,'']]]
 ];
