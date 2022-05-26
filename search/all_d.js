2cb4ee9e83b545d93270e998d00d97704400771c
bbernhar <bbernhar@users.noreply.github.com>
2022-05-26 17:19:07 +0000

    deploy: 3856b4d1c6f6440648b9afd3484028cb64a35477

--- search/all_d.js
+++ search/all_d.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['name_0',['name',['../struct_trace_file.html#a86e9dc6e842f180b7a7b6e3ad5f4bd2c',1,'TraceFile']]],
-  ['neverallocate_1',['NeverAllocate',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a7d7b04df4ac858359500cccb4795981a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
-  ['next_2',['next',['../classgpgmm_1_1_link_node.html#af130013a33a0163a6dcf4515825a7ab8',1,'gpgmm::LinkNode']]],
-  ['nextpoweroftwo_3',['NextPowerOfTwo',['../namespacegpgmm.html#a7105ddb398cb2ce2e5b8c5823cb12ec7',1,'gpgmm']]],
-  ['noncopyable_4',['NonCopyable',['../classgpgmm_1_1_non_copyable.html',1,'gpgmm::NonCopyable'],['../classgpgmm_1_1_non_copyable.html#a588c58b8330a3d62608141c07a4132bf',1,'gpgmm::NonCopyable::NonCopyable()=default'],['../classgpgmm_1_1_non_copyable.html#a87e9480f0d92752ea967c83b79c064b8',1,'gpgmm::NonCopyable::NonCopyable(NonCopyable &amp;&amp;)=default']]],
-  ['noncopyable_2eh_5',['NonCopyable.h',['../_non_copyable_8h.html',1,'']]],
-  ['numofhits_6',['NumOfHits',['../structgpgmm_1_1_cache_stats.html#a81aa18f541e3b8cc43806234a969b689',1,'gpgmm::CacheStats']]],
-  ['numofmisses_7',['NumOfMisses',['../structgpgmm_1_1_cache_stats.html#a9331f38d59d151a56f6a70542d2c4a93',1,'gpgmm::CacheStats']]]
+  ['name_1',['Name',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a0516834446dd159f231149e122460c62',1,'gpgmm::d3d12::HEAP_INFO::Name()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#ae6834399de2553f39466107cac078ee9',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::Name()']]],
+  ['neverallocate_2',['NeverAllocate',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a7d7b04df4ac858359500cccb4795981a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
+  ['next_3',['next',['../classgpgmm_1_1_link_node.html#af130013a33a0163a6dcf4515825a7ab8',1,'gpgmm::LinkNode']]],
+  ['nextpoweroftwo_4',['NextPowerOfTwo',['../namespacegpgmm.html#a7105ddb398cb2ce2e5b8c5823cb12ec7',1,'gpgmm']]],
+  ['noncopyable_5',['NonCopyable',['../classgpgmm_1_1_non_copyable.html',1,'gpgmm::NonCopyable'],['../classgpgmm_1_1_non_copyable.html#a588c58b8330a3d62608141c07a4132bf',1,'gpgmm::NonCopyable::NonCopyable()=default'],['../classgpgmm_1_1_non_copyable.html#a87e9480f0d92752ea967c83b79c064b8',1,'gpgmm::NonCopyable::NonCopyable(NonCopyable &amp;&amp;)=default']]],
+  ['noncopyable_2eh_6',['NonCopyable.h',['../_non_copyable_8h.html',1,'']]],
+  ['numofhits_7',['NumOfHits',['../structgpgmm_1_1_cache_stats.html#a81aa18f541e3b8cc43806234a969b689',1,'gpgmm::CacheStats']]],
+  ['numofmisses_8',['NumOfMisses',['../structgpgmm_1_1_cache_stats.html#a9331f38d59d151a56f6a70542d2c4a93',1,'gpgmm::CacheStats']]]
 ];
--- search/variables_c.js
+++ search/variables_c.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['name_0',['name',['../struct_trace_file.html#a86e9dc6e842f180b7a7b6e3ad5f4bd2c',1,'TraceFile']]],
-  ['neverallocate_1',['NeverAllocate',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a7d7b04df4ac858359500cccb4795981a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
-  ['numofhits_2',['NumOfHits',['../structgpgmm_1_1_cache_stats.html#a81aa18f541e3b8cc43806234a969b689',1,'gpgmm::CacheStats']]],
-  ['numofmisses_3',['NumOfMisses',['../structgpgmm_1_1_cache_stats.html#a9331f38d59d151a56f6a70542d2c4a93',1,'gpgmm::CacheStats']]]
+  ['name_1',['Name',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a0516834446dd159f231149e122460c62',1,'gpgmm::d3d12::HEAP_INFO::Name()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#ae6834399de2553f39466107cac078ee9',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::Name()']]],
+  ['neverallocate_2',['NeverAllocate',['../structgpgmm_1_1_m_e_m_o_r_y___a_l_l_o_c_a_t_i_o_n___r_e_q_u_e_s_t.html#a7d7b04df4ac858359500cccb4795981a',1,'gpgmm::MEMORY_ALLOCATION_REQUEST']]],
+  ['numofhits_3',['NumOfHits',['../structgpgmm_1_1_cache_stats.html#a81aa18f541e3b8cc43806234a969b689',1,'gpgmm::CacheStats']]],
+  ['numofmisses_4',['NumOfMisses',['../structgpgmm_1_1_cache_stats.html#a9331f38d59d151a56f6a70542d2c4a93',1,'gpgmm::CacheStats']]]
 ];
