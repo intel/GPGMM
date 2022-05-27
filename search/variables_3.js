2aa7037b466b481b2f0285f849de401bd6fb836a
bbernhar <bbernhar@users.noreply.github.com>
2022-05-27 04:47:20 +0000

    deploy: 7f14588f0b91b0376a5e930311f5435813ac2b8c

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['debugname_0',['DebugName',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a157727e245a1f06f8f49df65ecae35ab',1,'gpgmm::d3d12::HEAP_INFO::DebugName()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#ada0b80ec1ce7223878b30d61e6742b8f',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::DebugName()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aa306a32673748802f822de9821c8c8b1',1,'gpgmm::d3d12::ALLOCATION_DESC::DebugName()']]],
+  ['debugname_0',['DebugName',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#ada0b80ec1ce7223878b30d61e6742b8f',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::DebugName()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aa306a32673748802f822de9821c8c8b1',1,'gpgmm::d3d12::ALLOCATION_DESC::DebugName()']]],
   ['description_1',['Description',['../structgpgmm_1_1_l_o_g___m_e_s_s_a_g_e.html#aa33ae066a7eef4e4129dfde15679510b',1,'gpgmm::LOG_MESSAGE']]],
   ['destroybuffer_2',['DestroyBuffer',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a5b38432f6894b225397bb4acfa31ad44',1,'gpgmm::vk::VulkanFunctions']]],
   ['destroyimage_3',['DestroyImage',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#ac75229aa0618a442f8e37ac3cfcd6f0f',1,'gpgmm::vk::VulkanFunctions']]],
