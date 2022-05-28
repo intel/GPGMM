bb66d2a76532965eb8398256316a672477a29a04
bbernhar <bbernhar@users.noreply.github.com>
2022-05-28 21:32:54 +0000

    deploy: 5301ce39a630100d520c9a6bbd316b00fde70f0b

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['description_0',['Description',['../structgpgmm_1_1_l_o_g___m_e_s_s_a_g_e.html#aa33ae066a7eef4e4129dfde15679510b',1,'gpgmm::LOG_MESSAGE']]],
-  ['destroybuffer_1',['DestroyBuffer',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a5b38432f6894b225397bb4acfa31ad44',1,'gpgmm::vk::VulkanFunctions']]],
-  ['destroyimage_2',['DestroyImage',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#ac75229aa0618a442f8e37ac3cfcd6f0f',1,'gpgmm::vk::VulkanFunctions']]],
-  ['device_3',['device',['../structgpgmm_1_1vk_1_1_gp_create_allocator_info.html#aa7358241e3fe2e757151f23df6648526',1,'gpgmm::vk::GpCreateAllocatorInfo']]],
-  ['device_4',['Device',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a7fad963cc70495b611e255860a1aeaf3',1,'gpgmm::d3d12::RESIDENCY_DESC::Device()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#ac532e66c935cc87bc431f1e0f0564175',1,'gpgmm::d3d12::ALLOCATOR_DESC::Device()']]]
+  ['debugname_0',['DebugName',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#ada0b80ec1ce7223878b30d61e6742b8f',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::DebugName()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a1bf383e2f509558fd16cbf1c3aba0bd3',1,'gpgmm::d3d12::ALLOCATION_DESC::DebugName()']]],
+  ['description_1',['Description',['../structgpgmm_1_1_l_o_g___m_e_s_s_a_g_e.html#aa33ae066a7eef4e4129dfde15679510b',1,'gpgmm::LOG_MESSAGE']]],
+  ['destroybuffer_2',['DestroyBuffer',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a5b38432f6894b225397bb4acfa31ad44',1,'gpgmm::vk::VulkanFunctions']]],
+  ['destroyimage_3',['DestroyImage',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#ac75229aa0618a442f8e37ac3cfcd6f0f',1,'gpgmm::vk::VulkanFunctions']]],
+  ['device_4',['Device',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a7fad963cc70495b611e255860a1aeaf3',1,'gpgmm::d3d12::RESIDENCY_DESC::Device()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#ac532e66c935cc87bc431f1e0f0564175',1,'gpgmm::d3d12::ALLOCATOR_DESC::Device()']]],
+  ['device_5',['device',['../structgpgmm_1_1vk_1_1_gp_create_allocator_info.html#aa7358241e3fe2e757151f23df6648526',1,'gpgmm::vk::GpCreateAllocatorInfo']]]
 ];
