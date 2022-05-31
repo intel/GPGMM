5eaa50fab8d8d1179fa10daafee938b5f3ee1fb8
bbernhar <bbernhar@users.noreply.github.com>
2022-05-31 18:18:14 +0000

    deploy: e07f4c31eaf77bb577fc91db99d5ed2948a14055

--- search/classes_3.js
+++ search/classes_3.js
@@ -4,13 +4,12 @@ var searchData=
   ['d3d12residencymanagertests_1',['D3D12ResidencyManagerTests',['../class_d3_d12_residency_manager_tests.html',1,'']]],
   ['d3d12resourceallocatortests_2',['D3D12ResourceAllocatorTests',['../class_d3_d12_resource_allocator_tests.html',1,'']]],
   ['d3d12testbase_3',['D3D12TestBase',['../classgpgmm_1_1d3d12_1_1_d3_d12_test_base.html',1,'gpgmm::d3d12']]],
-  ['debugobject_4',['DebugObject',['../classgpgmm_1_1d3d12_1_1_debug_object.html',1,'gpgmm::d3d12']]],
-  ['debugplatform_5',['DebugPlatform',['../classgpgmm_1_1_debug_platform.html',1,'gpgmm']]],
-  ['debugresourceallocator_6',['DebugResourceAllocator',['../classgpgmm_1_1d3d12_1_1_debug_resource_allocator.html',1,'gpgmm::d3d12']]],
-  ['desc_7',['Desc',['../structgpgmm_1_1_desc.html',1,'gpgmm']]],
-  ['devicememory_8',['DeviceMemory',['../classgpgmm_1_1vk_1_1_device_memory.html',1,'gpgmm::vk']]],
-  ['devicememoryallocator_9',['DeviceMemoryAllocator',['../classgpgmm_1_1vk_1_1_device_memory_allocator.html',1,'gpgmm::vk']]],
-  ['dummybuddyblockallocator_10',['DummyBuddyBlockAllocator',['../class_dummy_buddy_block_allocator.html',1,'']]],
-  ['dummymemoryallocator_11',['DummyMemoryAllocator',['../classgpgmm_1_1_dummy_memory_allocator.html',1,'gpgmm']]],
-  ['dummyobject_12',['DummyObject',['../class_dummy_object.html',1,'']]]
+  ['debugplatform_4',['DebugPlatform',['../classgpgmm_1_1_debug_platform.html',1,'gpgmm']]],
+  ['debugresourceallocator_5',['DebugResourceAllocator',['../classgpgmm_1_1d3d12_1_1_debug_resource_allocator.html',1,'gpgmm::d3d12']]],
+  ['desc_6',['Desc',['../structgpgmm_1_1_desc.html',1,'gpgmm']]],
+  ['devicememory_7',['DeviceMemory',['../classgpgmm_1_1vk_1_1_device_memory.html',1,'gpgmm::vk']]],
+  ['devicememoryallocator_8',['DeviceMemoryAllocator',['../classgpgmm_1_1vk_1_1_device_memory_allocator.html',1,'gpgmm::vk']]],
+  ['dummybuddyblockallocator_9',['DummyBuddyBlockAllocator',['../class_dummy_buddy_block_allocator.html',1,'']]],
+  ['dummymemoryallocator_10',['DummyMemoryAllocator',['../classgpgmm_1_1_dummy_memory_allocator.html',1,'gpgmm']]],
+  ['dummyobject_11',['DummyObject',['../class_dummy_object.html',1,'']]]
 ];
