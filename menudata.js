f31462d296bf723cfe27b0989252c893648cea98
bbernhar <bbernhar@users.noreply.github.com>
2022-06-01 18:31:53 +0000

    deploy: a05806fe24e0bcf3fa5ce16a6f089390fa3a2fde

--- menudata.js
+++ menudata.js
@@ -74,7 +74,8 @@ var menudata={children:[
 {text:"k",url:"namespacemembers_eval.html#index_k"},
 {text:"m",url:"namespacemembers_eval.html#index_m"},
 {text:"n",url:"namespacemembers_eval.html#index_n"},
-{text:"o",url:"namespacemembers_eval.html#index_o"}]}]}]},
+{text:"o",url:"namespacemembers_eval.html#index_o"},
+{text:"r",url:"namespacemembers_eval.html#index_r"}]}]}]},
 {text:"Classes",url:"annotated.html",children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
