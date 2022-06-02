f282b045db52c8745b5e124609253214e90c9de2
bbernhar <bbernhar@users.noreply.github.com>
2022-06-02 21:59:22 +0000

    deploy: 65aa1e4b8f48d6f8898e597e52925c1f09344cfa

--- search/classes_e.js
+++ search/classes_e.js
@@ -2,11 +2,10 @@ var searchData=
 [
   ['refcounted_0',['RefCounted',['../classgpgmm_1_1_ref_counted.html',1,'gpgmm']]],
   ['residency_5fdesc_1',['RESIDENCY_DESC',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html',1,'gpgmm::d3d12']]],
-  ['residency_5fmanager_5finfo_2',['RESIDENCY_MANAGER_INFO',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___i_n_f_o.html',1,'gpgmm::d3d12']]],
-  ['residencymanager_3',['ResidencyManager',['../classgpgmm_1_1d3d12_1_1_residency_manager.html',1,'gpgmm::d3d12']]],
-  ['residencyset_4',['ResidencySet',['../classgpgmm_1_1d3d12_1_1_residency_set.html',1,'gpgmm::d3d12']]],
-  ['resource_5fallocation_5finfo_5',['RESOURCE_ALLOCATION_INFO',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html',1,'gpgmm::d3d12']]],
-  ['resourceallocation_6',['ResourceAllocation',['../classgpgmm_1_1d3d12_1_1_resource_allocation.html',1,'gpgmm::d3d12']]],
-  ['resourceallocator_7',['ResourceAllocator',['../classgpgmm_1_1d3d12_1_1_resource_allocator.html',1,'gpgmm::d3d12']]],
-  ['resourceheapallocator_8',['ResourceHeapAllocator',['../classgpgmm_1_1d3d12_1_1_resource_heap_allocator.html',1,'gpgmm::d3d12']]]
+  ['residencymanager_2',['ResidencyManager',['../classgpgmm_1_1d3d12_1_1_residency_manager.html',1,'gpgmm::d3d12']]],
+  ['residencyset_3',['ResidencySet',['../classgpgmm_1_1d3d12_1_1_residency_set.html',1,'gpgmm::d3d12']]],
+  ['resource_5fallocation_5finfo_4',['RESOURCE_ALLOCATION_INFO',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html',1,'gpgmm::d3d12']]],
+  ['resourceallocation_5',['ResourceAllocation',['../classgpgmm_1_1d3d12_1_1_resource_allocation.html',1,'gpgmm::d3d12']]],
+  ['resourceallocator_6',['ResourceAllocator',['../classgpgmm_1_1d3d12_1_1_resource_allocator.html',1,'gpgmm::d3d12']]],
+  ['resourceheapallocator_7',['ResourceHeapAllocator',['../classgpgmm_1_1d3d12_1_1_resource_heap_allocator.html',1,'gpgmm::d3d12']]]
 ];
