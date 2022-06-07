3b1c9ea3d5eeee3775de4faaa9e4f30ced7614e7
Bryan Bernhart <bryan.bernhart@intel.com>
2022-06-07 09:06:40 -0700

    Use DebugEvent for OOM warning in SlabMemoryAllocator.

--- src/gpgmm/common/SlabMemoryAllocator.cpp
+++ src/gpgmm/common/SlabMemoryAllocator.cpp
@@ -102,8 +102,8 @@ namespace gpgmm {
         // Otherwise, creating a larger slab will page-out smaller slabs.
         if (availableForAllocation < slabSize) {
             const uint64_t slabSizeUnderBudget = FindNextFreeSlabOfSize(requestSize);
-            DebugLog() << "Unable to use slab size due to available memory: ("
-                       << slabSizeUnderBudget << " vs " << slabSize << " bytes).";
+            DebugEvent(GetTypename()) << "Unable to use slab size due to available memory: ("
+                                      << slabSizeUnderBudget << " vs " << slabSize << " bytes).";
             slabSize = slabSizeUnderBudget;
         }
 
