15e8829e6f8ffd36de77a16134465e630b8f6a42
bbernhar <bbernhar@users.noreply.github.com>
2022-06-14 16:10:11 +0000

    deploy: f596a327c9a4c533262cc065b921da5398ec083f

--- search/all_7.js
+++ search/all_7.js
@@ -8,6 +8,6 @@ var searchData=
   ['heap_5finfo_5',['HEAP_INFO',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html',1,'gpgmm::d3d12']]],
   ['heapd3d12_2ecpp_6',['HeapD3D12.cpp',['../_heap_d3_d12_8cpp.html',1,'']]],
   ['heapd3d12_2eh_7',['HeapD3D12.h',['../_heap_d3_d12_8h.html',1,'']]],
-  ['heapoffset_8',['HeapOffset',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a0160d7cd2425e8e144e4924c19abd517',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO']]],
+  ['heapoffset_8',['HeapOffset',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aeeba45039b3912861fd6cd09099abd8e',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC']]],
   ['heaptype_9',['HeapType',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aa720936c4215d487a3e391e144836691',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
 ];
