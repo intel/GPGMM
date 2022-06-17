22de292a32563a6e05e9efc5f7b2976ff3fc8b17
bbernhar <bbernhar@users.noreply.github.com>
2022-06-17 22:41:53 +0000

    deploy: 07877a1b07a72bb0c6a41079b937bfe832330cae

--- search/variables_9.js
+++ search/variables_9.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['kidrefkey_0',['kIdRefKey',['../classgpgmm_1_1_trace_event_i_d.html#a5b728e7a6173a22afcd0b631c47ffbb9',1,'gpgmm::TraceEventID']]],
-  ['knoid_1',['kNoId',['../_trace_event_8h.html#a66921627f287947ace92fdf52ef8bd6c',1,'TraceEvent.h']]]
+  ['knoid_1',['kNoId',['../_trace_event_8h.html#a66921627f287947ace92fdf52ef8bd6c',1,'TraceEvent.h']]],
+  ['kpoweroftwocachesizes_2',['kPowerOfTwoCacheSizes',['../classgpgmm_1_1_memory_size.html#a092e109c012fbf86aca66d9e391f7576',1,'gpgmm::MemorySize']]],
+  ['kpoweroftwoclasssize_3',['kPowerOfTwoClassSize',['../classgpgmm_1_1_memory_size.html#a87d91cc4cf01e2772b6359a94d85f1c7',1,'gpgmm::MemorySize']]]
 ];
