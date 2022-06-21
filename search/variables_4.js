7fe04cdf1e9ca7eb47bedc3abb0365e6180f4e7c
bbernhar <bbernhar@users.noreply.github.com>
2022-06-21 00:48:28 +0000

    deploy: e10c10266d54ece386001bdce49b8709e0b8c59b

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['eventscope_0',['EventScope',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___r_e_c_o_r_d___o_p_t_i_o_n_s.html#ae169d62c949f95574f3e2209a90400fe',1,'gpgmm::d3d12::ALLOCATOR_RECORD_OPTIONS']]],
-  ['evictbatchsize_1',['EvictBatchSize',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a53a630fb905d3b72bbeffe1b00030b82',1,'gpgmm::d3d12::RESIDENCY_DESC::EvictBatchSize()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a1b89fa5ae0e519ba4f8414db8f3d725b',1,'gpgmm::d3d12::ALLOCATOR_DESC::EvictBatchSize()']]]
+  ['evictbatchsize_1',['EvictBatchSize',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a53a630fb905d3b72bbeffe1b00030b82',1,'gpgmm::d3d12::RESIDENCY_DESC::EvictBatchSize()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a1b89fa5ae0e519ba4f8414db8f3d725b',1,'gpgmm::d3d12::ALLOCATOR_DESC::EvictBatchSize()']]],
+  ['extrarequiredheapflags_2',['ExtraRequiredHeapFlags',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a08cf139c6ad730e0dae3df0526ce36bd',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
 ];
