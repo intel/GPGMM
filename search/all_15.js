fabe3b39b680672a2232b5dffa42be2c7ecfcda2
bbernhar <bbernhar@users.noreply.github.com>
2022-06-21 22:34:53 +0000

    deploy: 070e2f0a6fd50a3af89909ac72282fb8990e6357

--- search/all_15.js
+++ search/all_15.js
@@ -1,15 +1,16 @@
 var searchData=
 [
-  ['value_0',['value',['../classgpgmm_1_1_link_node.html#afe622ef885cedad3f561d51d7255f5f3',1,'gpgmm::LinkNode::value() const'],['../classgpgmm_1_1_link_node.html#a5239e24be03daa6ce6e6ffe06b9e872f',1,'gpgmm::LinkNode::value()']]],
-  ['value_5ftype_1',['value_type',['../classgpgmm_1_1_flags.html#a7571944b56f71c3dc84fd4ec54efc9b5',1,'gpgmm::Flags']]],
-  ['videomemorybudget_2',['VideoMemoryBudget',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a2968f221adfd367a56db90b17b457594',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
-  ['vk_5fplatform_2eh_3',['vk_platform.h',['../vk__platform_8h.html',1,'']]],
-  ['vkresourceallocatortests_4',['VKResourceAllocatorTests',['../class_v_k_resource_allocator_tests.html',1,'']]],
-  ['vkresourceallocatortests_2ecpp_5',['VKResourceAllocatorTests.cpp',['../_v_k_resource_allocator_tests_8cpp.html',1,'']]],
-  ['vktest_2ecpp_6',['VKTest.cpp',['../_v_k_test_8cpp.html',1,'']]],
-  ['vktest_2eh_7',['VKTest.h',['../_v_k_test_8h.html',1,'']]],
-  ['vktestbase_8',['VKTestBase',['../classgpgmm_1_1vk_1_1_v_k_test_base.html',1,'gpgmm::vk']]],
-  ['voidcallback_9',['VoidCallback',['../classgpgmm_1_1_void_callback.html',1,'gpgmm']]],
-  ['vulkanapiversion_10',['vulkanApiVersion',['../structgpgmm_1_1vk_1_1_gp_create_allocator_info.html#a87944635e273d90ec24a2b2646653ce8',1,'gpgmm::vk::GpCreateAllocatorInfo']]],
-  ['vulkanfunctions_11',['VulkanFunctions',['../structgpgmm_1_1vk_1_1_vulkan_functions.html',1,'gpgmm::vk']]]
+  ['validaterequest_0',['ValidateRequest',['../classgpgmm_1_1_memory_allocator.html#a530611a84164ed2ee7434790dc1d1fec',1,'gpgmm::MemoryAllocator']]],
+  ['value_1',['value',['../classgpgmm_1_1_link_node.html#afe622ef885cedad3f561d51d7255f5f3',1,'gpgmm::LinkNode::value() const'],['../classgpgmm_1_1_link_node.html#a5239e24be03daa6ce6e6ffe06b9e872f',1,'gpgmm::LinkNode::value()']]],
+  ['value_5ftype_2',['value_type',['../classgpgmm_1_1_flags.html#a7571944b56f71c3dc84fd4ec54efc9b5',1,'gpgmm::Flags']]],
+  ['videomemorybudget_3',['VideoMemoryBudget',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a2968f221adfd367a56db90b17b457594',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
+  ['vk_5fplatform_2eh_4',['vk_platform.h',['../vk__platform_8h.html',1,'']]],
+  ['vkresourceallocatortests_5',['VKResourceAllocatorTests',['../class_v_k_resource_allocator_tests.html',1,'']]],
+  ['vkresourceallocatortests_2ecpp_6',['VKResourceAllocatorTests.cpp',['../_v_k_resource_allocator_tests_8cpp.html',1,'']]],
+  ['vktest_2ecpp_7',['VKTest.cpp',['../_v_k_test_8cpp.html',1,'']]],
+  ['vktest_2eh_8',['VKTest.h',['../_v_k_test_8h.html',1,'']]],
+  ['vktestbase_9',['VKTestBase',['../classgpgmm_1_1vk_1_1_v_k_test_base.html',1,'gpgmm::vk']]],
+  ['voidcallback_10',['VoidCallback',['../classgpgmm_1_1_void_callback.html',1,'gpgmm']]],
+  ['vulkanapiversion_11',['vulkanApiVersion',['../structgpgmm_1_1vk_1_1_gp_create_allocator_info.html#a87944635e273d90ec24a2b2646653ce8',1,'gpgmm::vk::GpCreateAllocatorInfo']]],
+  ['vulkanfunctions_12',['VulkanFunctions',['../structgpgmm_1_1vk_1_1_vulkan_functions.html',1,'gpgmm::vk']]]
 ];
