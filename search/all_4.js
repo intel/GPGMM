3f701b598f9e690334106c8db040af06e76a3b73
bbernhar <bbernhar@users.noreply.github.com>
2022-06-24 20:36:29 +0000

    deploy: ea4a5f3593a026c49ca246a0ac93691802eaf02b

--- search/all_4.js
+++ search/all_4.js
@@ -25,7 +25,7 @@ var searchData=
   ['eventtracewriter_2ecpp_22',['EventTraceWriter.cpp',['../_event_trace_writer_8cpp.html',1,'']]],
   ['eventtracewriter_2eh_23',['EventTraceWriter.h',['../_event_trace_writer_8h.html',1,'']]],
   ['evict_24',['Evict',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#a5dc4f4b070eb246d079e6701b7ab6cc4',1,'gpgmm::d3d12::ResidencyManager']]],
-  ['evictbatchsize_25',['EvictBatchSize',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a53a630fb905d3b72bbeffe1b00030b82',1,'gpgmm::d3d12::RESIDENCY_DESC::EvictBatchSize()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a1b89fa5ae0e519ba4f8414db8f3d725b',1,'gpgmm::d3d12::ALLOCATOR_DESC::EvictBatchSize()']]],
+  ['evictbatchsize_25',['EvictBatchSize',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a53a630fb905d3b72bbeffe1b00030b82',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
   ['execute_5fcommand_5flists_5fdesc_26',['EXECUTE_COMMAND_LISTS_DESC',['../structgpgmm_1_1d3d12_1_1_e_x_e_c_u_t_e___c_o_m_m_a_n_d___l_i_s_t_s___d_e_s_c.html',1,'gpgmm::d3d12']]],
   ['executecommandlists_27',['ExecuteCommandLists',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#ab889d80e34d4ebca85727887ac42f982',1,'gpgmm::d3d12::ResidencyManager']]],
   ['expect_5ffailed_28',['EXPECT_FAILED',['../_d3_d12_test_8h.html#a42e6fdcab733c6e579810c9d001665b7',1,'D3D12Test.h']]],
