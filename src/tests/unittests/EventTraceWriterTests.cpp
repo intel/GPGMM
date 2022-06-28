74e100738ee59ea2585cb39f27417dc35c0fc39d
Bryan B <bryan.bernhart@intel.com>
2022-06-27 20:29:11 -0700

    Fix AV upon merging events from exited thread. (#456)
    Fixes crash upon merging events from other threads that exited. The event buffer per thread reference in TLS was dangling after thread exit. This fix replaces the use of the TLS pointer with a RAII type which knows to unlinks itself once exit/destruct.

---
+++ src/tests/unittests/EventTraceWriterTests.cpp
@@ -0,0 +1,61 @@
+// Copyright 2021 The GPGMM Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "gpgmm/common/TraceEvent.h"
+
+#include <thread>
+#include <vector>
+
+static constexpr const char* kDummyTrace = "DummyTrace.json";
+
+using namespace gpgmm;
+
+class EventTraceWriterTests : public testing::Test {
+  public:
+    void SetUp() override {
+        StartupEventTrace(kDummyTrace, TraceEventPhase::None, /*flushOnDestruct*/ true);
+    }
+
+    void TearDown() override {
+        ShutdownEventTrace();
+    }
+};
+
+TEST_F(EventTraceWriterTests, SingleThreadWrites) {
+    constexpr uint32_t kEventCount = 64u;
+    for (size_t i = 0; i < kEventCount; i++) {
+        TRACE_EVENT_INSTANT0(TraceEventCategory::Default, "InstantEvent");
+    }
+
+    // 1 event per thread + 1 metadata event for main thread name.
+    EXPECT_EQ(GetQueuedEventsForTesting(), 64 + 1u);
+}
+
+TEST_F(EventTraceWriterTests, MultiThreadWrites) {
+    constexpr uint32_t kThreadCount = 64u;
+    std::vector<std::thread> threads(kThreadCount);
+    for (size_t threadIdx = 0; threadIdx < threads.size(); threadIdx++) {
+        threads[threadIdx] = std::thread(
+            [&]() { TRACE_EVENT_INSTANT0(TraceEventCategory::Default, "InstantEvent"); });
+    }
+
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
+
+    // 1 event per thread + 1 metadata event for main thread name.
+    EXPECT_EQ(GetQueuedEventsForTesting(), 64 + 1u);
+}
