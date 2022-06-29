5f2d894b737278700e14f198cc3b53cd91019f5c
bbernhar <bbernhar@users.noreply.github.com>
2022-06-29 18:33:25 +0000

    deploy: 419e9528dd06508ec562354bea13e73f8b09cce3

--- search/functions_15.js
+++ search/functions_15.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['wait_0',['Wait',['../classgpgmm_1_1_memory_allocation_event.html#aa02ea1c7db4704d3c26d48528edc4f96',1,'gpgmm::MemoryAllocationEvent::Wait()'],['../classgpgmm_1_1_async_event_impl.html#acb5fc8871e847abbe6c88eeb972c8edc',1,'gpgmm::AsyncEventImpl::Wait()'],['../classgpgmm_1_1_event.html#a2e9d6fac62e85956e99c69735faf8725',1,'gpgmm::Event::Wait()'],['../classgpgmm_1_1d3d12_1_1_budget_update_event.html#af9d99f32101ea56fb2ed32947fee9a27',1,'gpgmm::d3d12::BudgetUpdateEvent::Wait()']]],
+  ['wait_0',['Wait',['../classgpgmm_1_1_memory_allocation_event.html#aa02ea1c7db4704d3c26d48528edc4f96',1,'gpgmm::MemoryAllocationEvent::Wait()'],['../classgpgmm_1_1_async_event_impl.html#acb5fc8871e847abbe6c88eeb972c8edc',1,'gpgmm::AsyncEventImpl::Wait()'],['../classgpgmm_1_1_event.html#a2e9d6fac62e85956e99c69735faf8725',1,'gpgmm::Event::Wait()']]],
   ['waitfor_1',['WaitFor',['../classgpgmm_1_1d3d12_1_1_fence.html#a1adcdae2029b716a5fd77c1f4e6bdfa3',1,'gpgmm::d3d12::Fence']]],
   ['warnevent_2',['WarnEvent',['../namespacegpgmm.html#aea958111e401705339ddcc41d50c099e',1,'gpgmm']]],
   ['warninglog_3',['WarningLog',['../namespacegpgmm.html#aa84ded2f91df21fa9e7e9b425ba0044f',1,'gpgmm']]],
