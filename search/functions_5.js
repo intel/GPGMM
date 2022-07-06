55eb246edb152c802e9ca37d4944a75ed87995c6
bbernhar <bbernhar@users.noreply.github.com>
2022-07-06 19:13:56 +0000

    deploy: 4febd0657751dd3e143c1f4068a5cfae280d6d01

--- search/functions_5.js
+++ search/functions_5.js
@@ -4,5 +4,6 @@ var searchData=
   ['findsegment_1',['FindSegment',['../namespacegpgmm.html#a32892309d78ee85a15a988b5138ee547',1,'gpgmm']]],
   ['flags_2',['Flags',['../classgpgmm_1_1_flags.html#a888bbdf58ef865b5a811de49091ca726',1,'gpgmm::Flags::Flags()'],['../classgpgmm_1_1_flags.html#a7ec454035c5e336e20f75b80528f3241',1,'gpgmm::Flags::Flags(EnumT flag)'],['../classgpgmm_1_1_flags.html#ad6b7489ece11fd6c55d08e7922830ecf',1,'gpgmm::Flags::Flags(ValueT value)']]],
   ['flushandremovebufferentry_3',['FlushAndRemoveBufferEntry',['../classgpgmm_1_1_event_trace_writer.html#a99d7387699f1bb2ce3b1c6684ae2f2bb',1,'gpgmm::EventTraceWriter']]],
-  ['flushqueuedeventstodisk_4',['FlushQueuedEventsToDisk',['../classgpgmm_1_1_event_trace_writer.html#a2ac2d778219848435278df097bc8a77e',1,'gpgmm::EventTraceWriter']]]
+  ['flusheventtracetodisk_4',['FlushEventTraceToDisk',['../namespacegpgmm.html#aceb4a4aff30c1e09b139c9272abc5afe',1,'gpgmm']]],
+  ['flushqueuedeventstodisk_5',['FlushQueuedEventsToDisk',['../classgpgmm_1_1_event_trace_writer.html#a2ac2d778219848435278df097bc8a77e',1,'gpgmm::EventTraceWriter']]]
 ];
