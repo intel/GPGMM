f4b17fce0492203def06826660ae1f3ac98a2472
bbernhar <bbernhar@users.noreply.github.com>
2022-07-08 17:57:44 +0000

    deploy: f99fd057598aa0e7c07d93412ebba8d6bb5f51be

--- search/functions_13.js
+++ search/functions_13.js
@@ -4,6 +4,5 @@ var searchData=
   ['unmap_1',['Unmap',['../classgpgmm_1_1d3d12_1_1_resource_allocation.html#aacb13e9781e92486cd62b08b39adf645',1,'gpgmm::d3d12::ResourceAllocation']]],
   ['unref_2',['Unref',['../classgpgmm_1_1_ref_counted.html#a850338d6a459a388bbbcd974a470831c',1,'gpgmm::RefCounted']]],
   ['unregisterandexit_3',['UnregisterAndExit',['../classgpgmm_1_1d3d12_1_1_budget_update_task.html#a2f04a3554e40c3b75ec8c9248b8aa5d5',1,'gpgmm::d3d12::BudgetUpdateTask::UnregisterAndExit()'],['../classgpgmm_1_1d3d12_1_1_budget_update_event.html#a96670ad9ba7b6c15cbacb07043e4777d',1,'gpgmm::d3d12::BudgetUpdateEvent::UnregisterAndExit()']]],
-  ['updateresidency_4',['UpdateResidency',['../classgpgmm_1_1d3d12_1_1_resource_allocation.html#a49f91f6c880a59b5ad7875a2a075c2cd',1,'gpgmm::d3d12::ResourceAllocation']]],
-  ['updatevideomemorysegments_5',['UpdateVideoMemorySegments',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#a6e75d0e53352b915a43de66b9ffd5237',1,'gpgmm::d3d12::ResidencyManager']]]
+  ['updatevideomemorysegments_4',['UpdateVideoMemorySegments',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#a6e75d0e53352b915a43de66b9ffd5237',1,'gpgmm::d3d12::ResidencyManager']]]
 ];
