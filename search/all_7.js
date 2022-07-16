68d3e774ceef73fca59d1733fd606d39ace25d44
bbernhar <bbernhar@users.noreply.github.com>
2022-07-16 18:49:58 +0000

    deploy: be5ff64c37e68ac12fdd6231abaffbfe013fbbd6

--- search/all_7.js
+++ search/all_7.js
@@ -4,7 +4,7 @@ var searchData=
   ['hasflags_1',['HasFlags',['../classgpgmm_1_1_flags.html#a3b5ef18766f4c8de2f1b1435f23ad2a0',1,'gpgmm::Flags']]],
   ['hasoneref_2',['HasOneRef',['../classgpgmm_1_1_ref_counted.html#a37f79c230eb0f82f20a8fe455f7664b8',1,'gpgmm::RefCounted']]],
   ['head_3',['head',['../classgpgmm_1_1_linked_list.html#a37254c01e28b0154513bb15d7b5ef923',1,'gpgmm::LinkedList']]],
-  ['heap_4',['Heap',['../classgpgmm_1_1d3d12_1_1_heap.html',1,'gpgmm::d3d12::Heap'],['../classgpgmm_1_1d3d12_1_1_heap.html#af8d4136d4b188da4cead2150ed0d6693',1,'gpgmm::d3d12::Heap::Heap()']]],
+  ['heap_4',['Heap',['../classgpgmm_1_1d3d12_1_1_heap.html',1,'gpgmm::d3d12']]],
   ['heap_5fdesc_5',['HEAP_DESC',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html',1,'gpgmm::d3d12']]],
   ['heap_5finfo_6',['HEAP_INFO',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html',1,'gpgmm::d3d12']]],
   ['heapd3d12_2ecpp_7',['HeapD3D12.cpp',['../_heap_d3_d12_8cpp.html',1,'']]],
