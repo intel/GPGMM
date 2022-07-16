4ae05d92d50656dbd7b191908a4e093afd520e11
bbernhar <bbernhar@users.noreply.github.com>
2022-07-16 18:36:45 +0000

    deploy: 2b35f12922e52ad048e851984f83073966737864

--- search/functions_7.js
+++ search/functions_7.js
@@ -3,5 +3,6 @@ var searchData=
   ['handleassertionfailure_0',['HandleAssertionFailure',['../namespacegpgmm.html#a6ad016a9b35300004a5bca19541c3dfe',1,'gpgmm']]],
   ['hasflags_1',['HasFlags',['../classgpgmm_1_1_flags.html#a3b5ef18766f4c8de2f1b1435f23ad2a0',1,'gpgmm::Flags']]],
   ['hasoneref_2',['HasOneRef',['../classgpgmm_1_1_ref_counted.html#a37f79c230eb0f82f20a8fe455f7664b8',1,'gpgmm::RefCounted']]],
-  ['head_3',['head',['../classgpgmm_1_1_linked_list.html#a37254c01e28b0154513bb15d7b5ef923',1,'gpgmm::LinkedList']]]
+  ['head_3',['head',['../classgpgmm_1_1_linked_list.html#a37254c01e28b0154513bb15d7b5ef923',1,'gpgmm::LinkedList']]],
+  ['heap_4',['Heap',['../classgpgmm_1_1d3d12_1_1_heap.html#af8d4136d4b188da4cead2150ed0d6693',1,'gpgmm::d3d12::Heap']]]
 ];
