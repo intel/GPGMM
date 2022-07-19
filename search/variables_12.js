ed3814a0d39577034343a57524f6c7a4a1cebe0c
bbernhar <bbernhar@users.noreply.github.com>
2022-07-19 01:41:51 +0000

    deploy: 3a2adc0b634e51734068951193f9af59d6f368db

--- search/variables_12.js
+++ search/variables_12.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['videomemorybudget_0',['VideoMemoryBudget',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a2968f221adfd367a56db90b17b457594',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
-  ['vulkanapiversion_1',['vulkanApiVersion',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#aad966715473066760a61bc34f1b8ad72',1,'gpgmm::vk::GpAllocatorCreateInfo']]]
+  ['vulkanapiversion_1',['vulkanApiVersion',['../structgpgmm_1_1vk_1_1_gp_create_allocator_info.html#a87944635e273d90ec24a2b2646653ce8',1,'gpgmm::vk::GpCreateAllocatorInfo']]]
 ];
