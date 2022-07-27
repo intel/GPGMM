af3e91f2b1130e52df65d8669f5bdee0e5bc06be
bbernhar <bbernhar@users.noreply.github.com>
2022-07-27 16:18:59 +0000

    deploy: 1a0e661f1f192f221a2f931accaf9b5c71e3edae

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['indexedmemorypool_0',['IndexedMemoryPool',['../classgpgmm_1_1_indexed_memory_pool.html',1,'gpgmm']]],
-  ['iterator_1',['iterator',['../structgpgmm_1_1_stable_list_1_1iterator.html',1,'gpgmm::StableList']]],
-  ['iunknownimpl_2',['IUnknownImpl',['../classgpgmm_1_1d3d12_1_1_i_unknown_impl.html',1,'gpgmm::d3d12']]]
+  ['iunknownimpl_1',['IUnknownImpl',['../classgpgmm_1_1d3d12_1_1_i_unknown_impl.html',1,'gpgmm::d3d12']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,16 +1,14 @@
 var searchData=
 [
-  ['emplace_5fback_0',['emplace_back',['../classgpgmm_1_1_stable_list.html#ad40f28071e997bfd95961a23cc469b4f',1,'gpgmm::StableList']]],
-  ['empty_1',['empty',['../classgpgmm_1_1_linked_list.html#ad448483bcf5a201c0b6f86fad43ade63',1,'gpgmm::LinkedList::empty()'],['../classgpgmm_1_1_stable_list.html#a254273922f5073c0565f42bc7417fc28',1,'gpgmm::StableList::empty()']]],
-  ['end_2',['end',['../classgpgmm_1_1_memory_cache.html#a11332292a65154e33968db6f41e3cbed',1,'gpgmm::MemoryCache::end()'],['../classgpgmm_1_1d3d12_1_1_residency_set.html#af9aa4b5e9164f9f13cf9a8b83ea0d4c0',1,'gpgmm::d3d12::ResidencySet::end()'],['../classgpgmm_1_1_linked_list.html#a307f546e0ace07f5e8aa05266cea2343',1,'gpgmm::LinkedList::end()'],['../classgpgmm_1_1_linked_list.html#acc6129c1df9dddf80e35790594298bbc',1,'gpgmm::LinkedList::end() const'],['../classgpgmm_1_1_stable_list.html#abf55035fdb8a6d06f6f5fab582570a55',1,'gpgmm::StableList::end()'],['../classgpgmm_1_1_stable_list.html#a25721f7a4d279825bb58a77615cfd3dd',1,'gpgmm::StableList::end() const']]],
-  ['endelapsedtime_3',['EndElapsedTime',['../classgpgmm_1_1_platform_time.html#abeb9c84a8f7c52149cb2f4c60b06033e',1,'gpgmm::PlatformTime::EndElapsedTime()'],['../classgpgmm_1_1_windows_time.html#a43efe1821064c19969745130f691201f',1,'gpgmm::WindowsTime::EndElapsedTime()']]],
-  ['endmemorycheck_4',['EndMemoryCheck',['../classgpgmm_1_1_debug_platform.html#a5f7216c35f2329f607987d73ba9c9a8d',1,'gpgmm::DebugPlatform::EndMemoryCheck()'],['../classgpgmm_1_1_windows_debug_platform.html#abe3f2c8d78b539caacabf9e69f831346',1,'gpgmm::WindowsDebugPlatform::EndMemoryCheck()']]],
-  ['enqueuetraceevent_5',['EnqueueTraceEvent',['../classgpgmm_1_1_event_trace_writer.html#a8519ae4cf10499656fc19486386f8bc3',1,'gpgmm::EventTraceWriter']]],
-  ['erase_6',['erase',['../classgpgmm_1_1_stable_list.html#a28f8ef3759e16856ea70a7a4715931af',1,'gpgmm::StableList::erase(size_t index)'],['../classgpgmm_1_1_stable_list.html#af650e4ac8cc4e5057b6f5363835049ae',1,'gpgmm::StableList::erase(const_iterator it)']]],
-  ['errorevent_7',['ErrorEvent',['../namespacegpgmm.html#a9ce85bdbbdfb1efebb93a6eea5f3db43',1,'gpgmm']]],
-  ['errorlog_8',['ErrorLog',['../namespacegpgmm.html#aebed7617bb2d08b0a2eac27f84cac24e',1,'gpgmm']]],
-  ['event_9',['Event',['../classgpgmm_1_1_event.html#afbb9549815e21e5ad23d97b2d20ce668',1,'gpgmm::Event']]],
-  ['eventmessage_10',['EventMessage',['../classgpgmm_1_1_event_message.html#a612aa757e09fa59401617c060ef1519a',1,'gpgmm::EventMessage::EventMessage(const LogSeverity &amp;level, const char *name, int messageId=EventMessageId::Unknown)'],['../classgpgmm_1_1_event_message.html#aee3e8d8f190d8ea04a72cb42c0c27d0b',1,'gpgmm::EventMessage::EventMessage(EventMessage &amp;&amp;other)=default']]],
-  ['eventtracewriter_11',['EventTraceWriter',['../classgpgmm_1_1_event_trace_writer.html#a5b34f87f224ac1ae0c21cdb00504499c',1,'gpgmm::EventTraceWriter']]],
-  ['executecommandlists_12',['ExecuteCommandLists',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#ab889d80e34d4ebca85727887ac42f982',1,'gpgmm::d3d12::ResidencyManager']]]
+  ['empty_0',['empty',['../classgpgmm_1_1_linked_list.html#ad448483bcf5a201c0b6f86fad43ade63',1,'gpgmm::LinkedList']]],
+  ['end_1',['end',['../classgpgmm_1_1_memory_cache.html#a11332292a65154e33968db6f41e3cbed',1,'gpgmm::MemoryCache::end()'],['../classgpgmm_1_1d3d12_1_1_residency_set.html#af9aa4b5e9164f9f13cf9a8b83ea0d4c0',1,'gpgmm::d3d12::ResidencySet::end()'],['../classgpgmm_1_1_linked_list.html#a307f546e0ace07f5e8aa05266cea2343',1,'gpgmm::LinkedList::end()'],['../classgpgmm_1_1_linked_list.html#acc6129c1df9dddf80e35790594298bbc',1,'gpgmm::LinkedList::end() const']]],
+  ['endelapsedtime_2',['EndElapsedTime',['../classgpgmm_1_1_platform_time.html#abeb9c84a8f7c52149cb2f4c60b06033e',1,'gpgmm::PlatformTime::EndElapsedTime()'],['../classgpgmm_1_1_windows_time.html#a43efe1821064c19969745130f691201f',1,'gpgmm::WindowsTime::EndElapsedTime()']]],
+  ['endmemorycheck_3',['EndMemoryCheck',['../classgpgmm_1_1_windows_debug_platform.html#abe3f2c8d78b539caacabf9e69f831346',1,'gpgmm::WindowsDebugPlatform::EndMemoryCheck()'],['../classgpgmm_1_1_debug_platform.html#a5f7216c35f2329f607987d73ba9c9a8d',1,'gpgmm::DebugPlatform::EndMemoryCheck()']]],
+  ['enqueuetraceevent_4',['EnqueueTraceEvent',['../classgpgmm_1_1_event_trace_writer.html#a8519ae4cf10499656fc19486386f8bc3',1,'gpgmm::EventTraceWriter']]],
+  ['errorevent_5',['ErrorEvent',['../namespacegpgmm.html#a9ce85bdbbdfb1efebb93a6eea5f3db43',1,'gpgmm']]],
+  ['errorlog_6',['ErrorLog',['../namespacegpgmm.html#aebed7617bb2d08b0a2eac27f84cac24e',1,'gpgmm']]],
+  ['event_7',['Event',['../classgpgmm_1_1_event.html#afbb9549815e21e5ad23d97b2d20ce668',1,'gpgmm::Event']]],
+  ['eventmessage_8',['EventMessage',['../classgpgmm_1_1_event_message.html#a612aa757e09fa59401617c060ef1519a',1,'gpgmm::EventMessage::EventMessage(const LogSeverity &amp;level, const char *name, int messageId=EventMessageId::Unknown)'],['../classgpgmm_1_1_event_message.html#aee3e8d8f190d8ea04a72cb42c0c27d0b',1,'gpgmm::EventMessage::EventMessage(EventMessage &amp;&amp;other)=default']]],
+  ['eventtracewriter_9',['EventTraceWriter',['../classgpgmm_1_1_event_trace_writer.html#a5b34f87f224ac1ae0c21cdb00504499c',1,'gpgmm::EventTraceWriter']]],
+  ['executecommandlists_10',['ExecuteCommandLists',['../classgpgmm_1_1d3d12_1_1_residency_manager.html#ab889d80e34d4ebca85727887ac42f982',1,'gpgmm::d3d12::ResidencyManager']]]
 ];
