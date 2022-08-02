d31d6db2632a7b83e9d67feeabd8fb5bcbdf19b6
bjjones <bjjones@users.noreply.github.com>
2022-08-02 01:43:58 +0000

    deploy: a4b48e701bec47e77a88d6709edd54647c55d5bb

--- search/typedefs_9.js
+++ search/typedefs_9.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['underlyingtype_0',['UnderlyingType',['../classgpgmm_1_1d3d12_1_1_residency_set.html#aa9ec54fe066278aa184581434d5a451d',1,'gpgmm::d3d12::ResidencySet']]]
+  ['underlyingtype_0',['UnderlyingType',['../classgpgmm_1_1d3d12_1_1_residency_list.html#a4002cc07dd9e7f1969f8c53b5dc5ba59',1,'gpgmm::d3d12::ResidencyList::UnderlyingType()'],['../classgpgmm_1_1d3d12_1_1_residency_set.html#aa9ec54fe066278aa184581434d5a451d',1,'gpgmm::d3d12::ResidencySet::UnderlyingType()']]]
 ];
--- search/variables_e.js
+++ search/variables_e.js
@@ -3,7 +3,7 @@ var searchData=
   ['recordoptions_0',['RecordOptions',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a315f4eb31901ec275f927904ec8c58f2',1,'gpgmm::d3d12::RESIDENCY_DESC::RecordOptions()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a3a83aa65ac3cb38e5ed6d94f6df12de2',1,'gpgmm::d3d12::ALLOCATOR_DESC::RecordOptions()']]],
   ['requestsizeinbytes_1',['RequestSizeInBytes',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a6349c2d4d91032e648e5712c2476ad77',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC']]],
   ['requiredpropertyflags_2',['requiredPropertyFlags',['../structgpgmm_1_1vk_1_1_gp_resource_allocation_create_info.html#ab9451843785b4203a40d23cea9fbeee9',1,'gpgmm::vk::GpResourceAllocationCreateInfo']]],
-  ['residencysets_3',['ResidencySets',['../structgpgmm_1_1d3d12_1_1_e_x_e_c_u_t_e___c_o_m_m_a_n_d___l_i_s_t_s___d_e_s_c.html#a535bdd3b27feed406f06ed476e465b16',1,'gpgmm::d3d12::EXECUTE_COMMAND_LISTS_DESC']]],
+  ['residencylists_3',['ResidencyLists',['../structgpgmm_1_1d3d12_1_1_e_x_e_c_u_t_e___c_o_m_m_a_n_d___l_i_s_t_s___d_e_s_c.html#a4601ce949ec7ff61656c5e6671c6f4c5',1,'gpgmm::d3d12::EXECUTE_COMMAND_LISTS_DESC']]],
   ['residentmemorycount_4',['ResidentMemoryCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___i_n_f_o.html#a0738ae7d06a21f1927588e66de79de49',1,'gpgmm::d3d12::RESIDENCY_INFO']]],
   ['residentmemoryusage_5',['ResidentMemoryUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___i_n_f_o.html#ab384facef1a981e982929326c565d3c1',1,'gpgmm::d3d12::RESIDENCY_INFO']]],
   ['resourcedescriptor_6',['resourceDescriptor',['../structgpgmm_1_1d3d12_1_1_c_r_e_a_t_e___r_e_s_o_u_r_c_e___d_e_s_c.html#a5be4f11fc109370ade0000671302ef08',1,'gpgmm::d3d12::CREATE_RESOURCE_DESC']]],
