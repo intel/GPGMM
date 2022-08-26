f1ce7bf3a16c66b6e0c4c13a5f60c2651e6df76f
Bryan Bernhart <bryan.bernhart@intel.com>
2022-08-26 16:41:29 -0700

    Make AllocationMethod::kUndefined default value.

--- src/gpgmm/common/MemoryAllocation.h
+++ src/gpgmm/common/MemoryAllocation.h
@@ -26,29 +26,29 @@ namespace gpgmm {
     */
     enum AllocationMethod {
 
+        /** \brief Not yet allocated or invalid.
+
+        This is an invalid state that assigned temporary before the actual method is known.
+        */
+        kUndefined = 0,
+
         /** \brief Not sub-divided.
 
         One and only one allocation exists for the memory.
         */
-        kStandalone = 0x0,
+        kStandalone = 1,
 
         /** \brief Sub-divided using one or more allocations.
 
         Underlying memory will be broken up into one or more memory allocations.
         */
-        kSubAllocated = 0x2,
+        kSubAllocated = 2,
 
         /** \brief Sub-divided within a single memory allocation.
 
         A single memory allocation will be broken into one or more sub-allocations.
         */
-        kSubAllocatedWithin = 0x4,
-
-        /** \brief Not yet allocated or invalid.
-
-        This is an invalid state that assigned temporary before the actual method is known.
-        */
-        kUndefined = 0x8
+        kSubAllocatedWithin = 3,
     };
 
     struct MemoryBlock;
