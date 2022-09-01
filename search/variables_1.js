9262a6b95fd0a60afec0bd541ed5902ac1ce4241
bbernhar <bbernhar@users.noreply.github.com>
2022-09-01 21:27:45 +0000

    deploy: ad435db1406f577c7e830242cf2f810872d0d16a

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['bindbuffermemory_0',['BindBufferMemory',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a24ad152a285a2ba1bd28eed880f745de',1,'gpgmm::vk::VulkanFunctions']]],
-  ['bindimagememory_1',['BindImageMemory',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#ac3945bbd6a69b7d805e69017d911eb65',1,'gpgmm::vk::VulkanFunctions']]]
+  ['bindimagememory_1',['BindImageMemory',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#ac3945bbd6a69b7d805e69017d911eb65',1,'gpgmm::vk::VulkanFunctions']]],
+  ['budget_2',['Budget',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#aac67b1916a22771093bc77268102f36b',1,'gpgmm::d3d12::RESIDENCY_DESC']]]
 ];
