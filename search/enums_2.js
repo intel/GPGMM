e37910e57a17cd38aea7802c5838f7d5e0c20940
bbernhar <bbernhar@users.noreply.github.com>
2022-09-06 18:43:58 +0000

    deploy: 43ce62e09cebea4c676ca0e5eef9ae5b129a764d

--- search/enums_2.js
+++ search/enums_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['event_5frecord_5fflags_0',['EVENT_RECORD_FLAGS',['../namespacegpgmm_1_1d3d12.html#aa3f15dccfce0d5328b19d533a8cfdd0b',1,'gpgmm::d3d12']]],
-  ['event_5frecord_5fscope_1',['EVENT_RECORD_SCOPE',['../namespacegpgmm_1_1d3d12.html#a50666648c6ef0535d4b9afed754af8c0',1,'gpgmm::d3d12']]],
+  ['event_5frecord_5fflags_0',['EVENT_RECORD_FLAGS',['../namespacegpgmm_1_1d3d12.html#aa3f15dccfce0d5328b19d533a8cfdd0b',1,'gpgmm::d3d12::EVENT_RECORD_FLAGS()'],['../namespacegpgmm_1_1d3d12.html#aa3f15dccfce0d5328b19d533a8cfdd0b',1,'gpgmm::d3d12::EVENT_RECORD_FLAGS()']]],
+  ['event_5frecord_5fscope_1',['EVENT_RECORD_SCOPE',['../namespacegpgmm_1_1d3d12.html#a50666648c6ef0535d4b9afed754af8c0',1,'gpgmm::d3d12::EVENT_RECORD_SCOPE()'],['../namespacegpgmm_1_1d3d12.html#a50666648c6ef0535d4b9afed754af8c0',1,'gpgmm::d3d12::EVENT_RECORD_SCOPE()']]],
   ['eventmessageid_2',['EventMessageId',['../namespacegpgmm.html#ada7d97a94732013a89cc316e53121088',1,'gpgmm']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['madapter_0',['mAdapter',['../classgpgmm_1_1d3d12_1_1_d3_d12_test_base.html#a5abe7985860427acf3da46c21711e83a',1,'gpgmm::d3d12::D3D12TestBase']]],
-  ['mallocator_1',['mAllocator',['../classgpgmm_1_1_memory_allocation.html#a1f5fddabf32874b04a4175515ab67ca2',1,'gpgmm::MemoryAllocation::mAllocator()'],['../class_dummy_buddy_block_allocator.html#a1a6219075023012a5cd9d0c1692c6ecc',1,'DummyBuddyBlockAllocator::mAllocator()']]],
+  ['mallocator_1',['mAllocator',['../classgpgmm_1_1_memory_allocation.html#a042c293174ad61e3b8638737cbc0b240',1,'gpgmm::MemoryAllocation::mAllocator()'],['../class_dummy_buddy_block_allocator.html#a1a6219075023012a5cd9d0c1692c6ecc',1,'DummyBuddyBlockAllocator::mAllocator()']]],
   ['mapmemory_2',['MapMemory',['../structgpgmm_1_1vk_1_1_vulkan_functions.html#a1ea9a9da65261c9a1d123a36e3b1376e',1,'gpgmm::vk::VulkanFunctions']]],
   ['maxbudgetinbytes_3',['MaxBudgetInBytes',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#aeed16a8f709d444e579bc42072e48888',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
   ['maxpctofvideomemorytobudget_4',['MaxPctOfVideoMemoryToBudget',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#ad5da443f0693ff19c707b8c280a950cf',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
@@ -9,10 +9,10 @@ var searchData=
   ['mcapturedmemorystats_6',['mCapturedMemoryStats',['../class_d3_d12_event_trace_replay.html#af8ccadde0ee707d67ebcfd2e0b50dada',1,'D3D12EventTraceReplay']]],
   ['mdata_7',['mData',['../structgpgmm_1_1_stable_list_1_1_chunk.html#a756710590d15e4891e3d252a67140787',1,'gpgmm::StableList::Chunk']]],
   ['mdevice_8',['mDevice',['../classgpgmm_1_1d3d12_1_1_d3_d12_test_base.html#aeb42dc59899ab2ac2962d0605dbd62fa',1,'gpgmm::d3d12::D3D12TestBase::mDevice()'],['../classgpgmm_1_1vk_1_1_v_k_test_base.html#adf045a03e4fc25461041068fdc15cccc',1,'gpgmm::vk::VKTestBase::mDevice()']]],
-  ['memoryfragmentationlimit_9',['MemoryFragmentationLimit',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#add08ae68e081a9159ffac3b2dde676d7',1,'gpgmm::d3d12::ALLOCATOR_DESC']]],
-  ['memoryfragmentationlimit_10',['memoryFragmentationLimit',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#ae0735f40746411baa653cb089b46f018',1,'gpgmm::vk::GpAllocatorCreateInfo']]],
-  ['memorygrowthfactor_11',['memoryGrowthFactor',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#aa56cf065cdc606967acec36e53b8d26b',1,'gpgmm::vk::GpAllocatorCreateInfo']]],
-  ['memorygrowthfactor_12',['MemoryGrowthFactor',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#aacc7ad0f5e83b2fc409ecdb32a9de228',1,'gpgmm::d3d12::ALLOCATOR_DESC']]],
+  ['memoryfragmentationlimit_9',['memoryFragmentationLimit',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#ae0735f40746411baa653cb089b46f018',1,'gpgmm::vk::GpAllocatorCreateInfo']]],
+  ['memoryfragmentationlimit_10',['MemoryFragmentationLimit',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#add08ae68e081a9159ffac3b2dde676d7',1,'gpgmm::d3d12::ALLOCATOR_DESC']]],
+  ['memorygrowthfactor_11',['MemoryGrowthFactor',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#aacc7ad0f5e83b2fc409ecdb32a9de228',1,'gpgmm::d3d12::ALLOCATOR_DESC']]],
+  ['memorygrowthfactor_12',['memoryGrowthFactor',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#aa56cf065cdc606967acec36e53b8d26b',1,'gpgmm::vk::GpAllocatorCreateInfo']]],
   ['memorysegmentgroup_13',['MemorySegmentGroup',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html#a235c2130bf71698d851998abd57f94dc',1,'gpgmm::d3d12::HEAP_DESC']]],
   ['method_14',['Method',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a8ab76e5fe780f00640a70c2fd27986c2',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC']]],
   ['mid_15',['mId',['../class_linked_list_tests_1_1_fake_object.html#afbc0f7fee706a8dbcf5b96f8c73f2ffc',1,'LinkedListTests::FakeObject']]],
