159eccd281dbf90f3b98788a989dd85084b8cbe0
bbernhar <bbernhar@users.noreply.github.com>
2022-09-08 17:05:03 +0000

    deploy: 1abad69a50fa022931cf66f22926f27537198541

--- search/enumvalues_5.js
+++ search/enumvalues_5.js
@@ -1,4 +1,15 @@
 var searchData=
 [
-  ['feature_5fresource_5fsuballocation_5fsupport_0',['FEATURE_RESOURCE_SUBALLOCATION_SUPPORT',['../namespacegpgmm_1_1d3d12.html#a8b9c8131ea6bc5d5142a117acc2f0fc4a5fabc9b62dba22dead49b098346cc133',1,'gpgmm::d3d12']]]
+  ['gp_5fallocation_5fcreate_5falways_5fprefetch_5fmemory_0',['GP_ALLOCATION_CREATE_ALWAYS_PREFETCH_MEMORY',['../namespacegpgmm_1_1vk.html#a3b98f42e0a18412e618eb4e0cc709879a3fcb49e1c85363cbbf59f0d3023ed97f',1,'gpgmm::vk']]],
+  ['gp_5fallocation_5fcreate_5fnever_5fallocate_5fmemory_1',['GP_ALLOCATION_CREATE_NEVER_ALLOCATE_MEMORY',['../namespacegpgmm_1_1vk.html#a3b98f42e0a18412e618eb4e0cc709879aa7d68fef0e42f5e38dea75cd4258a392',1,'gpgmm::vk']]],
+  ['gp_5fallocation_5fcreate_5fnever_5fsuballocate_5fmemory_2',['GP_ALLOCATION_CREATE_NEVER_SUBALLOCATE_MEMORY',['../namespacegpgmm_1_1vk.html#a3b98f42e0a18412e618eb4e0cc709879a1628e2a0aae1b470e5c3a656b96a06ad',1,'gpgmm::vk']]],
+  ['gp_5fallocation_5fcreate_5fnone_3',['GP_ALLOCATION_CREATE_NONE',['../namespacegpgmm_1_1vk.html#a3b98f42e0a18412e618eb4e0cc709879a659bc5049b8287ce7553855539f4277a',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5falgorithm_5fbuddy_5fsystem_4',['GP_ALLOCATOR_ALGORITHM_BUDDY_SYSTEM',['../namespacegpgmm_1_1vk.html#a59e276245b71c30952b1e8b14c65787ea5de2117df95c59919ce8963d49b0f4de',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5falgorithm_5ffixed_5fpool_5',['GP_ALLOCATOR_ALGORITHM_FIXED_POOL',['../namespacegpgmm_1_1vk.html#a59e276245b71c30952b1e8b14c65787eacdef595920add93787422ca014ae5aaf',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5falgorithm_5fsegmented_5fpool_6',['GP_ALLOCATOR_ALGORITHM_SEGMENTED_POOL',['../namespacegpgmm_1_1vk.html#a59e276245b71c30952b1e8b14c65787ea9869547137c9b4bae14485fec1b4243d',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5falgorithm_5fslab_7',['GP_ALLOCATOR_ALGORITHM_SLAB',['../namespacegpgmm_1_1vk.html#a59e276245b71c30952b1e8b14c65787ea266566240e02692c3a84e320a9f864ec',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5fcreate_5falways_5fin_5fbudget_8',['GP_ALLOCATOR_CREATE_ALWAYS_IN_BUDGET',['../namespacegpgmm_1_1vk.html#acbd246f80e3453eadf5bc945f88a44b6a2235b2b471afa3fef190680ab74e1e0f',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5fcreate_5falways_5fon_5fdemand_9',['GP_ALLOCATOR_CREATE_ALWAYS_ON_DEMAND',['../namespacegpgmm_1_1vk.html#acbd246f80e3453eadf5bc945f88a44b6a664ff79da77a56b4216620ca7668bbde',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5fcreate_5fdisable_5fmemory_5fprefetch_10',['GP_ALLOCATOR_CREATE_DISABLE_MEMORY_PREFETCH',['../namespacegpgmm_1_1vk.html#acbd246f80e3453eadf5bc945f88a44b6ac334c08000f123197b4920083c5af7a7',1,'gpgmm::vk']]],
+  ['gp_5fallocator_5fcreate_5fnone_11',['GP_ALLOCATOR_CREATE_NONE',['../namespacegpgmm_1_1vk.html#acbd246f80e3453eadf5bc945f88a44b6a6eedcff4846b79716a6ea0720996ab34',1,'gpgmm::vk']]]
 ];
