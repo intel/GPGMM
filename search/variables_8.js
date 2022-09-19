4c3f1923d11cdc1f10848a2959e932d313cc1a0b
bbernhar <bbernhar@users.noreply.github.com>
2022-09-19 17:12:04 +0000

    deploy: 08fa13bed4865474ba727c3b8de16ddadac4576b

--- search/variables_8.js
+++ search/variables_8.js
@@ -6,13 +6,14 @@ var searchData=
   ['initialfencevalue_3',['InitialFenceValue',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a6e773d9b18c48091bb224aba4980b603',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
   ['initialresourcestate_4',['initialResourceState',['../structgpgmm_1_1d3d12_1_1_c_r_e_a_t_e___r_e_s_o_u_r_c_e___d_e_s_c.html#ab4602b1a85f5c43af0465459e0bbb276',1,'gpgmm::d3d12::CREATE_RESOURCE_DESC']]],
   ['instance_5',['instance',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#a5d40707ce6865b7b4f8498b46a3fe7f3',1,'gpgmm::vk::GpAllocatorCreateInfo']]],
-  ['isallocationplaybackdisabled_6',['IsAllocationPlaybackDisabled',['../struct_test_enviroment_params.html#a48e65cd2e80202120c95a4eee879fc3b',1,'TestEnviromentParams']]],
-  ['isneverallocate_7',['IsNeverAllocate',['../struct_test_enviroment_params.html#a77965554f9ee8835d96a173b1203f380',1,'TestEnviromentParams']]],
-  ['isprefetchallowed_8',['IsPrefetchAllowed',['../struct_test_enviroment_params.html#a8fa6eff56d0e9a85c143415ac28afbe1',1,'TestEnviromentParams']]],
-  ['isresident_9',['IsResident',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a55c283d1377387927a69877f8dea317e',1,'gpgmm::d3d12::HEAP_INFO']]],
-  ['isresourceallocationwithincoherent_10',['IsResourceAllocationWithinCoherent',['../structgpgmm_1_1d3d12_1_1_f_e_a_t_u_r_e___d_a_t_a___r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___s_u_p_p_o_r_t.html#aac6965e75891cd2114c4ffa3837c29fe',1,'gpgmm::d3d12::FEATURE_DATA_RESOURCE_ALLOCATION_SUPPORT']]],
-  ['issamecapsrequired_11',['IsSameCapsRequired',['../struct_test_enviroment_params.html#a415b7adbe22fb3be53520d1b383dac5c',1,'TestEnviromentParams']]],
-  ['issuballocationdisabled_12',['IsSuballocationDisabled',['../struct_test_enviroment_params.html#ae993a2720189745cd81f0592a62882c0',1,'TestEnviromentParams']]],
-  ['isuma_13',['IsUMA',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a2f8e943fbe0d0ac61cb28acb1f58ccd7',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
-  ['iterations_14',['Iterations',['../struct_test_enviroment_params.html#ae4c7578c761300b96a59c74429ac3d8c',1,'TestEnviromentParams']]]
+  ['isallocatordisabled_6',['IsAllocatorDisabled',['../struct_test_enviroment_params.html#a53b9826edf311bcbd8110e6a610961da',1,'TestEnviromentParams']]],
+  ['isignorecapsmismatchenabled_7',['IsIgnoreCapsMismatchEnabled',['../struct_test_enviroment_params.html#a30edf0472f1dcc006bb733af6626f098',1,'TestEnviromentParams']]],
+  ['ismemorydisabled_8',['IsMemoryDisabled',['../struct_test_enviroment_params.html#af6cb82132434f5b390c302b5669e93a2',1,'TestEnviromentParams']]],
+  ['isneverallocate_9',['IsNeverAllocate',['../struct_test_enviroment_params.html#a77965554f9ee8835d96a173b1203f380',1,'TestEnviromentParams']]],
+  ['isprefetchallowed_10',['IsPrefetchAllowed',['../struct_test_enviroment_params.html#a8fa6eff56d0e9a85c143415ac28afbe1',1,'TestEnviromentParams']]],
+  ['isresident_11',['IsResident',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a55c283d1377387927a69877f8dea317e',1,'gpgmm::d3d12::HEAP_INFO']]],
+  ['isresourceallocationwithincoherent_12',['IsResourceAllocationWithinCoherent',['../structgpgmm_1_1d3d12_1_1_f_e_a_t_u_r_e___d_a_t_a___r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___s_u_p_p_o_r_t.html#aac6965e75891cd2114c4ffa3837c29fe',1,'gpgmm::d3d12::FEATURE_DATA_RESOURCE_ALLOCATION_SUPPORT']]],
+  ['issuballocationdisabled_13',['IsSuballocationDisabled',['../struct_test_enviroment_params.html#ae993a2720189745cd81f0592a62882c0',1,'TestEnviromentParams']]],
+  ['isuma_14',['IsUMA',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a2f8e943fbe0d0ac61cb28acb1f58ccd7',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
+  ['iterations_15',['Iterations',['../struct_test_enviroment_params.html#ae4c7578c761300b96a59c74429ac3d8c',1,'TestEnviromentParams']]]
 ];
