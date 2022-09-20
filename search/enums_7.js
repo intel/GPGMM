b76301974f9f58cae353b9668c728a0475f40751
bbernhar <bbernhar@users.noreply.github.com>
2022-09-20 19:09:18 +0000

    deploy: 0add4f9f10857b6b0bf4306487aa25b34ae4c9ce

--- search/enums_7.js
+++ search/enums_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['residency_5fflags_0',['RESIDENCY_FLAGS',['../namespacegpgmm_1_1d3d12.html#a91ac0725c909f9dae91c66c3bb049c73',1,'gpgmm::d3d12::RESIDENCY_FLAGS()'],['../namespacegpgmm_1_1d3d12.html#a91ac0725c909f9dae91c66c3bb049c73',1,'gpgmm::d3d12::RESIDENCY_FLAGS()']]],
-  ['residency_5fstatus_1',['RESIDENCY_STATUS',['../namespacegpgmm_1_1d3d12.html#a964c4ad3a4e304bf0e281459f327023c',1,'gpgmm::d3d12::RESIDENCY_STATUS()'],['../namespacegpgmm_1_1d3d12.html#a964c4ad3a4e304bf0e281459f327023c',1,'gpgmm::d3d12::RESIDENCY_STATUS()']]]
+  ['residency_5fflags_0',['RESIDENCY_FLAGS',['../namespacegpgmm_1_1d3d12.html#a91ac0725c909f9dae91c66c3bb049c73',1,'gpgmm::d3d12::RESIDENCY_FLAGS()'],['../namespacegpgmm_1_1d3d12.html#a91ac0725c909f9dae91c66c3bb049c73',1,'gpgmm::d3d12::RESIDENCY_FLAGS()']]]
 ];
--- search/enumvalues_d.js
+++ search/enumvalues_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['residency_5fflag_5fnever_5fupdate_5fbudget_5fon_5fworker_5fthread_0',['RESIDENCY_FLAG_NEVER_UPDATE_BUDGET_ON_WORKER_THREAD',['../namespacegpgmm_1_1d3d12.html#a91ac0725c909f9dae91c66c3bb049c73a8af39823a1abe244a290cd4a1a7b08dd',1,'gpgmm::d3d12']]],
-  ['residency_5fflag_5fnone_1',['RESIDENCY_FLAG_NONE',['../namespacegpgmm_1_1d3d12.html#a91ac0725c909f9dae91c66c3bb049c73a9d1fbe54850e1f746e6d76bad2e048df',1,'gpgmm::d3d12']]],
-  ['residency_5funknown_2',['RESIDENCY_UNKNOWN',['../namespacegpgmm_1_1d3d12.html#a964c4ad3a4e304bf0e281459f327023ca9f16cd6edc58dd7ce3fd392edb2697a2',1,'gpgmm::d3d12']]]
+  ['residency_5fflag_5fnone_1',['RESIDENCY_FLAG_NONE',['../namespacegpgmm_1_1d3d12.html#a91ac0725c909f9dae91c66c3bb049c73a9d1fbe54850e1f746e6d76bad2e048df',1,'gpgmm::d3d12']]]
 ];
