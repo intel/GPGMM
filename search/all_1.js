b55bf4b91f33d2133f8c0ebba0d2c750317564ac
bbernhar <bbernhar@users.noreply.github.com>
2022-10-18 01:32:42 +0000

    deploy: c87be121809667a2d89d0d83e1c732d6d6d643be

--- search/all_1.js
+++ search/all_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['checkfeaturesupport_0',['CheckFeatureSupport',['../namespacegpgmm_1_1d3d12.html#a6ffa8695a413186d32f9931dd8d3f6da',1,'gpgmm::d3d12']]],
   ['createheap_1',['CreateHeap',['../namespacegpgmm_1_1d3d12.html#ab752b370ed612a40fbbb135a6a2d5bfa',1,'gpgmm::d3d12']]],
   ['createheapfn_2',['CreateHeapFn',['../namespacegpgmm_1_1d3d12.html#a2e8c9e657d16843e8b6f296ad94af852',1,'gpgmm::d3d12']]],
-  ['createresidencylist_3',['CreateResidencyList',['../namespacegpgmm_1_1d3d12.html#a39709e589e0721620d76781805fa4a07',1,'gpgmm::d3d12']]],
+  ['createresidencylist_3',['CreateResidencyList',['../namespacegpgmm_1_1d3d12.html#a5f06db116d1746109ac7333dcc3411ee',1,'gpgmm::d3d12']]],
   ['createresidencymanager_4',['CreateResidencyManager',['../namespacegpgmm_1_1d3d12.html#a821820440ac096a41512b0008c9b6ce7',1,'gpgmm::d3d12']]],
   ['createresource_5',['CreateResource',['../namespacegpgmm_1_1d3d12.html#ac739d5fc673607a33e86b9361c79b2ac',1,'gpgmm::d3d12']]],
   ['createresourceallocator_6',['CreateResourceAllocator',['../namespacegpgmm_1_1d3d12.html#a9ababeded3a4c8d30d446d26cdaec39d',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, IResourceAllocator **ppResourceAllocatorOut, IResidencyManager **ppResidencyManagerOut)'],['../namespacegpgmm_1_1d3d12.html#a3a7284f0cf647fe791faefda54399854',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, IResidencyManager *pResidencyManager, IResourceAllocator **ppResourceAllocatorOut)']]],
