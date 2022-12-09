aec3c7f96e41d90079f5c6c70fb7c94cf3778cd7
bbernhar <bbernhar@users.noreply.github.com>
2022-12-09 01:31:00 +0000

    deploy: e44212914cb620bc2ab8c7c1183e51a2356d829e

--- search/all_10.js
+++ search/all_10.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['unlockheap_0',['UnlockHeap',['../namespacegpgmm_1_1d3d12.html#a89b7eaadd7c6e81676154b4656a0d7ad',1,'gpgmm::d3d12']]],
-  ['unmap_1',['Unmap',['../namespacegpgmm_1_1d3d12.html#a0f55ea2c417d3efdb1711fa0084b5864',1,'gpgmm::d3d12']]],
+  ['unmap_1',['Unmap',['../namespacegpgmm_1_1d3d12.html#a3cf28c7ae524e3045d4ef069a4d2c148',1,'gpgmm::d3d12']]],
   ['usedblockcount_2',['UsedBlockCount',['../structgpgmm_1_1_memory_allocator_stats.html#af2852fcd1fc395a8f6d58cd7e5972e94',1,'gpgmm::MemoryAllocatorStats']]],
   ['usedblockusage_3',['UsedBlockUsage',['../structgpgmm_1_1_memory_allocator_stats.html#aa6d375754fd50004ed5a6da5340db653',1,'gpgmm::MemoryAllocatorStats']]],
   ['usedetailedtimingevents_4',['UseDetailedTimingEvents',['../structgpgmm_1_1d3d12_1_1_e_v_e_n_t___r_e_c_o_r_d___o_p_t_i_o_n_s.html#afca8d6a4bec59c65a8b0cb233dcab0c9',1,'gpgmm::d3d12::EVENT_RECORD_OPTIONS']]],
