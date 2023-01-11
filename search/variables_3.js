3ddcf9774ac049b81e6fda25fa33dafaa8e442b1
bbernhar <bbernhar@users.noreply.github.com>
2023-01-11 02:34:13 +0000

    deploy: fc582073e22012723b2124d316a8fa31807206fd

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['eventscope_0',['EventScope',['../structgpgmm_1_1_e_v_e_n_t___r_e_c_o_r_d___o_p_t_i_o_n_s.html#a5611498b3b9e49c783e111511bdd0a94',1,'gpgmm::EVENT_RECORD_OPTIONS']]],
+  ['eventscope_0',['EventScope',['../structgpgmm_1_1d3d12_1_1_e_v_e_n_t___r_e_c_o_r_d___o_p_t_i_o_n_s.html#a896d8372fa79e87e2a2c095ba34059b7',1,'gpgmm::d3d12::EVENT_RECORD_OPTIONS']]],
   ['evictsizeinbytes_1',['EvictSizeInBytes',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#ab114ef99d3f1af9c49f493c1004b315f',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
   ['extrarequiredheapflags_2',['ExtraRequiredHeapFlags',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a08cf139c6ad730e0dae3df0526ce36bd',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
 ];
--- search/variables_d.js
+++ search/variables_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['usedblockcount_0',['UsedBlockCount',['../structgpgmm_1_1_memory_allocator_stats.html#af2852fcd1fc395a8f6d58cd7e5972e94',1,'gpgmm::MemoryAllocatorStats']]],
   ['usedblockusage_1',['UsedBlockUsage',['../structgpgmm_1_1_memory_allocator_stats.html#aa6d375754fd50004ed5a6da5340db653',1,'gpgmm::MemoryAllocatorStats']]],
-  ['usedetailedtimingevents_2',['UseDetailedTimingEvents',['../structgpgmm_1_1_e_v_e_n_t___r_e_c_o_r_d___o_p_t_i_o_n_s.html#a81763b598a748100e3d76312eea400fd',1,'gpgmm::EVENT_RECORD_OPTIONS']]],
+  ['usedetailedtimingevents_2',['UseDetailedTimingEvents',['../structgpgmm_1_1d3d12_1_1_e_v_e_n_t___r_e_c_o_r_d___o_p_t_i_o_n_s.html#afca8d6a4bec59c65a8b0cb233dcab0c9',1,'gpgmm::d3d12::EVENT_RECORD_OPTIONS']]],
   ['usedmemorycount_3',['UsedMemoryCount',['../structgpgmm_1_1_memory_allocator_stats.html#a11bd21bacec54bd989c7974ab428f8e1',1,'gpgmm::MemoryAllocatorStats']]],
   ['usedmemoryusage_4',['UsedMemoryUsage',['../structgpgmm_1_1_memory_allocator_stats.html#abb58fda10c080339690079f6bd7c22a6',1,'gpgmm::MemoryAllocatorStats']]]
 ];
