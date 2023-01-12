02787939cff6423c1b944c50e0d2431896c7ac90
bbernhar <bbernhar@users.noreply.github.com>
2023-01-12 21:03:27 +0000

    deploy: fc1ffbabd8a8f60eaa8f178c1fc4467d5d3c2823

--- search/all_1.js
+++ search/all_1.js
@@ -5,7 +5,7 @@ var searchData=
   ['createheapfn_2',['CreateHeapFn',['../namespacegpgmm_1_1d3d12.html#abdad89ada60fe0ff753062bf90695782',1,'gpgmm::d3d12']]],
   ['createresidencylist_3',['CreateResidencyList',['../namespacegpgmm_1_1d3d12.html#a5f06db116d1746109ac7333dcc3411ee',1,'gpgmm::d3d12']]],
   ['createresidencymanager_4',['CreateResidencyManager',['../namespacegpgmm_1_1d3d12.html#a821820440ac096a41512b0008c9b6ce7',1,'gpgmm::d3d12']]],
-  ['createresource_5',['CreateResource',['../namespacegpgmm_1_1d3d12.html#a1c46f070043b15d73f162e85ad07cf60',1,'gpgmm::d3d12']]],
+  ['createresource_5',['CreateResource',['../namespacegpgmm_1_1d3d12.html#ac739d5fc673607a33e86b9361c79b2ac',1,'gpgmm::d3d12']]],
   ['createresourceallocator_6',['CreateResourceAllocator',['../namespacegpgmm_1_1d3d12.html#a9ababeded3a4c8d30d446d26cdaec39d',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, IResourceAllocator **ppResourceAllocatorOut, IResidencyManager **ppResidencyManagerOut)'],['../namespacegpgmm_1_1d3d12.html#a3a7284f0cf647fe791faefda54399854',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, IResidencyManager *pResidencyManager, IResourceAllocator **ppResourceAllocatorOut)']]],
   ['currentmemorycount_7',['CurrentMemoryCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___s_t_a_t_s.html#ad01b4b74f7bd33e0fe403edfdcbdb28a',1,'gpgmm::d3d12::RESIDENCY_STATS']]],
   ['currentmemoryusage_8',['CurrentMemoryUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___s_t_a_t_s.html#aacf21f154fb7a4b4bc717b5dd74a63e7',1,'gpgmm::d3d12::RESIDENCY_STATS']]]
--- search/functions_0.js
+++ search/functions_0.js
@@ -4,6 +4,6 @@ var searchData=
   ['createheap_1',['CreateHeap',['../namespacegpgmm_1_1d3d12.html#aad821ee97af32ef90001f001682a43f0',1,'gpgmm::d3d12']]],
   ['createresidencylist_2',['CreateResidencyList',['../namespacegpgmm_1_1d3d12.html#a5f06db116d1746109ac7333dcc3411ee',1,'gpgmm::d3d12']]],
   ['createresidencymanager_3',['CreateResidencyManager',['../namespacegpgmm_1_1d3d12.html#a821820440ac096a41512b0008c9b6ce7',1,'gpgmm::d3d12']]],
-  ['createresource_4',['CreateResource',['../namespacegpgmm_1_1d3d12.html#a1c46f070043b15d73f162e85ad07cf60',1,'gpgmm::d3d12']]],
+  ['createresource_4',['CreateResource',['../namespacegpgmm_1_1d3d12.html#ac739d5fc673607a33e86b9361c79b2ac',1,'gpgmm::d3d12']]],
   ['createresourceallocator_5',['CreateResourceAllocator',['../namespacegpgmm_1_1d3d12.html#a9ababeded3a4c8d30d446d26cdaec39d',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, IResourceAllocator **ppResourceAllocatorOut, IResidencyManager **ppResidencyManagerOut)'],['../namespacegpgmm_1_1d3d12.html#a3a7284f0cf647fe791faefda54399854',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, IResidencyManager *pResidencyManager, IResourceAllocator **ppResourceAllocatorOut)']]]
 ];
