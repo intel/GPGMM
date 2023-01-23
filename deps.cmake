80c9f7c93a777863f8e1a26971458af6b8b886ee
Bernhart, Bryan <bryan.bernhart@intel.com>
2023-01-23 09:53:19 -0800

    Move revisions used by cmake fetchcontent into depfile.

---
+++ deps.cmake
@@ -0,0 +1,20 @@
+# Copyright 2022 The GPGMM Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Current revision of source-packages under third_party.
+set(GPGMM_GOOGLETEST_REVISION 6b74da4757a549563d7c37c8fae3e704662a043b)
+set(GPGMM_JSONCPP_REVISION 9059f5cad030ba11d37818847443a53918c327b1)
+set(GPGMM_BENCHMARK_REVISION f730846b0a3c0dc0699978846fb14ffb2fad0bdc)
+set(GPGMM_VULKAN_HEADERS_REVISION 9f73b931f402f23554a60015924e7e35c7047487)
+set(GPGMM_VULKAN_LOADER_REVISION 9a45e5a4ab79b81e27e1417ca65d53ecfe57ba37)
