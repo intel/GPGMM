ae111d6e090a82c2ef5968f028fcbd4a32ec614b
bbernhar <bbernhar@users.noreply.github.com>
2023-01-24 21:55:19 +0000

    deploy: 4e2fc8b4bfd7cde80573827872641f94c7802544

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['setdebugname_0',['SetDebugName',['../namespacegpgmm_1_1d3d12.html#a445f08a49a91decaacc53bc978572451',1,'gpgmm::d3d12']]],
-  ['setresidencystate_1',['SetResidencyState',['../namespacegpgmm_1_1d3d12.html#a471d5c4b88bc07e24aecc5c653915188',1,'gpgmm::d3d12']]],
-  ['setvideomemoryreservation_2',['SetVideoMemoryReservation',['../namespacegpgmm_1_1d3d12.html#adf07988a22cc662db5f53c8687d69c9d',1,'gpgmm::d3d12']]]
+  ['setvideomemoryreservation_1',['SetVideoMemoryReservation',['../namespacegpgmm_1_1d3d12.html#adf07988a22cc662db5f53c8687d69c9d',1,'gpgmm::d3d12']]]
 ];
