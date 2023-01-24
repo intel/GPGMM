a7697e8aa16e1e970d5ed1d235af41692aa0125a
bbernhar <bbernhar@users.noreply.github.com>
2023-01-24 21:12:33 +0000

    deploy: a83badc63855fb78c7286361cc314e25e84ab77d

--- search/all_e.js
+++ search/all_e.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['setdebugname_0',['SetDebugName',['../namespacegpgmm_1_1d3d12.html#a445f08a49a91decaacc53bc978572451',1,'gpgmm::d3d12']]],
-  ['setvideomemoryreservation_1',['SetVideoMemoryReservation',['../namespacegpgmm_1_1d3d12.html#adf07988a22cc662db5f53c8687d69c9d',1,'gpgmm::d3d12']]],
-  ['sizecachehits_2',['SizeCacheHits',['../structgpgmm_1_1_memory_allocator_stats.html#a53c5bbe2ec007e151e6cfdc7a3c5182a',1,'gpgmm::MemoryAllocatorStats']]],
-  ['sizecachemisses_3',['SizeCacheMisses',['../structgpgmm_1_1_memory_allocator_stats.html#ae9746d6cf9d0b091e93a163c30a149e7',1,'gpgmm::MemoryAllocatorStats']]],
-  ['sizeinbytes_4',['SizeInBytes',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a097b9ed7d8b4612d359036f3b18a1cc4',1,'gpgmm::d3d12::HEAP_INFO::SizeInBytes()'],['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html#a2389164925b57ec3d13631a8e659dc2f',1,'gpgmm::d3d12::HEAP_DESC::SizeInBytes()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aea8dd5cadda9d7c1af0ab968667f6571',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC::SizeInBytes()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a69a8397e54b68d7e4d816b4a3743f480',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::SizeInBytes()']]],
-  ['status_5',['Status',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#abccb4fe93b87d4e6b60bbb8a7c7742c4',1,'gpgmm::d3d12::HEAP_INFO']]],
-  ['suballocationalgorithm_6',['subAllocationAlgorithm',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#aa9c9a7ecf364ca57862369e3e6e2136f',1,'gpgmm::vk::GpAllocatorCreateInfo']]],
-  ['suballocationalgorithm_7',['SubAllocationAlgorithm',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#ad243d65ad29d052d9081094c6e608cbd',1,'gpgmm::d3d12::ALLOCATOR_DESC']]]
+  ['setresidencystate_1',['SetResidencyState',['../namespacegpgmm_1_1d3d12.html#a471d5c4b88bc07e24aecc5c653915188',1,'gpgmm::d3d12']]],
+  ['setvideomemoryreservation_2',['SetVideoMemoryReservation',['../namespacegpgmm_1_1d3d12.html#adf07988a22cc662db5f53c8687d69c9d',1,'gpgmm::d3d12']]],
+  ['sizecachehits_3',['SizeCacheHits',['../structgpgmm_1_1_memory_allocator_stats.html#a53c5bbe2ec007e151e6cfdc7a3c5182a',1,'gpgmm::MemoryAllocatorStats']]],
+  ['sizecachemisses_4',['SizeCacheMisses',['../structgpgmm_1_1_memory_allocator_stats.html#ae9746d6cf9d0b091e93a163c30a149e7',1,'gpgmm::MemoryAllocatorStats']]],
+  ['sizeinbytes_5',['SizeInBytes',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a097b9ed7d8b4612d359036f3b18a1cc4',1,'gpgmm::d3d12::HEAP_INFO::SizeInBytes()'],['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html#a2389164925b57ec3d13631a8e659dc2f',1,'gpgmm::d3d12::HEAP_DESC::SizeInBytes()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aea8dd5cadda9d7c1af0ab968667f6571',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC::SizeInBytes()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a69a8397e54b68d7e4d816b4a3743f480',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::SizeInBytes()']]],
+  ['status_6',['Status',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#abccb4fe93b87d4e6b60bbb8a7c7742c4',1,'gpgmm::d3d12::HEAP_INFO']]],
+  ['suballocationalgorithm_7',['subAllocationAlgorithm',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#aa9c9a7ecf364ca57862369e3e6e2136f',1,'gpgmm::vk::GpAllocatorCreateInfo']]],
+  ['suballocationalgorithm_8',['SubAllocationAlgorithm',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#ad243d65ad29d052d9081094c6e608cbd',1,'gpgmm::d3d12::ALLOCATOR_DESC']]]
 ];
