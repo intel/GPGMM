b1869e08be8168bf4729ba911c959ac53feba799
bbernhar <bbernhar@users.noreply.github.com>
2023-01-24 23:18:51 +0000

    deploy: e8b963ec65a188436c35b0e61c29a094942d8725

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['adapter_0',['Adapter',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#af36130c1c5c13528871e33b57d13911c',1,'gpgmm::d3d12::RESIDENCY_DESC::Adapter()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a017d4dcb6c2df114409f28b4e701de78',1,'gpgmm::d3d12::ALLOCATOR_DESC::Adapter()']]],
+  ['adapter_0',['Adapter',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#adc17fbd10471426bfff2908e7ba0e399',1,'gpgmm::d3d12::RESIDENCY_DESC::Adapter()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a15e0af38d2f44b185ada17a88415cf31',1,'gpgmm::d3d12::ALLOCATOR_DESC::Adapter()']]],
   ['alignment_1',['Alignment',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a44cd6b66437ee36cd251d7c04cc481f9',1,'gpgmm::d3d12::HEAP_INFO::Alignment()'],['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html#aad452e7c7a2ddd7bb683403bf50d3dec',1,'gpgmm::d3d12::HEAP_DESC::Alignment()'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a87d1ca40bafa2a2cd92363d0f2db7b8b',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO::Alignment()']]]
 ];
