6605f670f42c68b3157bd966262a208d6b807ceb
bbernhar <bbernhar@users.noreply.github.com>
2023-02-02 01:30:14 +0000

    deploy: 7bd14f5f68b494c362a58fe3270809a2ada40995

--- search/variables_6.js
+++ search/variables_6.js
@@ -11,6 +11,5 @@ var searchData=
   ['iresourceallocator_8',['IResourceAllocator',['../namespacegpgmm_1_1d3d12.html#adc19a7e39d7932a3c59ab30f069dc5f8',1,'gpgmm::d3d12']]],
   ['iscachedforresidency_9',['IsCachedForResidency',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a9e2b69dfacb81ee8b0f9ab9c9f62553d',1,'gpgmm::d3d12::HEAP_INFO']]],
   ['islocked_10',['IsLocked',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___i_n_f_o.html#a690c1fc9d71e8d5e13a3385b10e45872',1,'gpgmm::d3d12::HEAP_INFO']]],
-  ['isresourceallocationwithincoherent_11',['IsResourceAllocationWithinCoherent',['../structgpgmm_1_1d3d12_1_1_f_e_a_t_u_r_e___d_a_t_a___r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___s_u_p_p_o_r_t.html#aac6965e75891cd2114c4ffa3837c29fe',1,'gpgmm::d3d12::FEATURE_DATA_RESOURCE_ALLOCATION_SUPPORT']]],
-  ['isuma_12',['IsUMA',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a2f8e943fbe0d0ac61cb28acb1f58ccd7',1,'gpgmm::d3d12::RESIDENCY_DESC']]]
+  ['isresourceallocationwithincoherent_11',['IsResourceAllocationWithinCoherent',['../structgpgmm_1_1d3d12_1_1_f_e_a_t_u_r_e___d_a_t_a___r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___s_u_p_p_o_r_t.html#aac6965e75891cd2114c4ffa3837c29fe',1,'gpgmm::d3d12::FEATURE_DATA_RESOURCE_ALLOCATION_SUPPORT']]]
 ];
