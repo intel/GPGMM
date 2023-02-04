f5b4ef5173d27f10c500419b3c04a16c63859796
Bernhart, Bryan <bryan.bernhart@intel.com>
2023-02-03 19:13:33 -0800

    Revert "Add third_party\ninja to .gitignore"
    This reverts commit df892257c80c4aeba429312ba920a1650a735e90.

--- src/gpgmm/common/MemoryAllocator.cpp
+++ src/gpgmm/common/MemoryAllocator.cpp
@@ -147,10 +147,6 @@ namespace gpgmm {
         return kInvalidSize;
     }
 
-     uint64_t MemoryAllocator::GetMaxMemorySize() const {
-        return kInvalidSize;
-     }
-
     uint64_t MemoryAllocator::GetMemoryAlignment() const {
         return kNoRequiredAlignment;
     }
