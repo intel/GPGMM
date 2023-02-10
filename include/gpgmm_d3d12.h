9a1438208b0d31e3e52605a34a6442686a14a924
Bryan B <bryan.bernhart@intel.com>
2023-02-10 16:54:31 +0000

    Fix gpgmm.h enums failing builds due to #758. (#760)
    Moves back emum flag operation definitions to gpgmm namespace to avoid build errors. In a future change, these enums will be backend-specific.

--- include/gpgmm_d3d12.h
+++ include/gpgmm_d3d12.h
@@ -37,6 +37,10 @@
 
 #define GPGMM_INTERFACE struct
 
+namespace gpgmm {
+    DEFINE_ENUM_FLAG_OPERATORS(EventRecordFlags)  // Provided by <windows.h>
+}
+
 namespace gpgmm::d3d12 {
 
     /** \brief Debug object associates additional information for D3D objects using SetPrivateData.
@@ -276,10 +280,7 @@ namespace gpgmm::d3d12 {
         virtual HRESULT Reset() = 0;
     };
 
-    using EVENT_RECORD_FLAGS = EventRecordFlags;
     using EVENT_RECORD_OPTIONS = EventRecordOptions;
-
-    DEFINE_ENUM_FLAG_OPERATORS(EVENT_RECORD_FLAGS)
 
     /** \brief  Create a residency list or collection of heaps to manage together for residency.
 
