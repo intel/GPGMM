b287ad3438741997254064ab8d71ab4d04ccabdc
bbernhar <bbernhar@users.noreply.github.com>
2023-02-10 18:49:55 +0000

    deploy: ba0cafa3f5aaebc2d98100d4d663e3442c49cf28

--- search/all_3.js
+++ search/all_3.js
@@ -1,12 +1,11 @@
 var searchData=
 [
-  ['event_5frecord_5fflags_0',['EVENT_RECORD_FLAGS',['../namespacegpgmm_1_1d3d12.html#a1f163248588e3c8b50aa7f0573df60ed',1,'gpgmm::d3d12']]],
-  ['event_5frecord_5foptions_1',['EVENT_RECORD_OPTIONS',['../namespacegpgmm_1_1d3d12.html#a3a3c46bbf11c31a5c4bf6b480a275189',1,'gpgmm::d3d12']]],
-  ['eventrecordflags_2',['EventRecordFlags',['../namespacegpgmm.html#a3711a91511b2c0d45cee47f5b72a122a',1,'gpgmm']]],
-  ['eventrecordoptions_3',['EventRecordOptions',['../structgpgmm_1_1_event_record_options.html',1,'gpgmm']]],
-  ['eventrecordscope_4',['EventRecordScope',['../namespacegpgmm.html#ad9daa5c143c4edacad74b78b198d5c19',1,'gpgmm']]],
-  ['eventscope_5',['EventScope',['../structgpgmm_1_1_event_record_options.html#a077ffd44a45c62a7ceba888474450414',1,'gpgmm::EventRecordOptions']]],
-  ['evictsizeinbytes_6',['EvictSizeInBytes',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#ab114ef99d3f1af9c49f493c1004b315f',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
-  ['executecommandlists_7',['ExecuteCommandLists',['../namespacegpgmm_1_1d3d12.html#a0cccf47f2fa1cced8b6e3f88a85a13ca',1,'gpgmm::d3d12']]],
-  ['extrarequiredheapflags_8',['ExtraRequiredHeapFlags',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a08cf139c6ad730e0dae3df0526ce36bd',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
+  ['event_5frecord_5foptions_0',['EVENT_RECORD_OPTIONS',['../namespacegpgmm_1_1d3d12.html#a3a3c46bbf11c31a5c4bf6b480a275189',1,'gpgmm::d3d12']]],
+  ['eventrecordflags_1',['EventRecordFlags',['../namespacegpgmm.html#a3711a91511b2c0d45cee47f5b72a122a',1,'gpgmm']]],
+  ['eventrecordoptions_2',['EventRecordOptions',['../structgpgmm_1_1_event_record_options.html',1,'gpgmm']]],
+  ['eventrecordscope_3',['EventRecordScope',['../namespacegpgmm.html#ad9daa5c143c4edacad74b78b198d5c19',1,'gpgmm']]],
+  ['eventscope_4',['EventScope',['../structgpgmm_1_1_event_record_options.html#a077ffd44a45c62a7ceba888474450414',1,'gpgmm::EventRecordOptions']]],
+  ['evictsizeinbytes_5',['EvictSizeInBytes',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#ab114ef99d3f1af9c49f493c1004b315f',1,'gpgmm::d3d12::RESIDENCY_DESC']]],
+  ['executecommandlists_6',['ExecuteCommandLists',['../namespacegpgmm_1_1d3d12.html#a0cccf47f2fa1cced8b6e3f88a85a13ca',1,'gpgmm::d3d12']]],
+  ['extrarequiredheapflags_7',['ExtraRequiredHeapFlags',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a08cf139c6ad730e0dae3df0526ce36bd',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
 ];
--- search/typedefs_1.js
+++ search/typedefs_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['event_5frecord_5fflags_0',['EVENT_RECORD_FLAGS',['../namespacegpgmm_1_1d3d12.html#a1f163248588e3c8b50aa7f0573df60ed',1,'gpgmm::d3d12']]],
-  ['event_5frecord_5foptions_1',['EVENT_RECORD_OPTIONS',['../namespacegpgmm_1_1d3d12.html#a3a3c46bbf11c31a5c4bf6b480a275189',1,'gpgmm::d3d12']]]
+  ['event_5frecord_5foptions_0',['EVENT_RECORD_OPTIONS',['../namespacegpgmm_1_1d3d12.html#a3a3c46bbf11c31a5c4bf6b480a275189',1,'gpgmm::d3d12']]]
 ];
