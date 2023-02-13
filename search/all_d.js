8416c9868113a89ef6505b455f2960cceb00f55a
bbernhar <bbernhar@users.noreply.github.com>
2023-02-13 18:28:00 +0000

    deploy: be0a6a01f08ad42987fd7b4df5bc8def20372829

--- search/all_d.js
+++ search/all_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['recordoptions_0',['RecordOptions',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html#a315f4eb31901ec275f927904ec8c58f2',1,'gpgmm::d3d12::RESIDENCY_DESC::RecordOptions()'],['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___d_e_s_c.html#a3a83aa65ac3cb38e5ed6d94f6df12de2',1,'gpgmm::d3d12::ALLOCATOR_DESC::RecordOptions()']]],
-  ['releasememory_1',['ReleaseMemory',['../namespacegpgmm_1_1d3d12.html#a207c4b1488008518f0304784582cf52a',1,'gpgmm::d3d12']]],
+  ['releaseresourceheaps_1',['ReleaseResourceHeaps',['../namespacegpgmm_1_1d3d12.html#a06cd29b01b8c7a94760d503ca7282022',1,'gpgmm::d3d12']]],
   ['requiredpropertyflags_2',['requiredPropertyFlags',['../structgpgmm_1_1vk_1_1_gp_resource_allocation_create_info.html#ab9451843785b4203a40d23cea9fbeee9',1,'gpgmm::vk::GpResourceAllocationCreateInfo']]],
   ['requireresourceheappadding_3',['RequireResourceHeapPadding',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aed41cef6134f5552d5581f75d526683c',1,'gpgmm::d3d12::ALLOCATION_DESC']]],
   ['reset_4',['Reset',['../namespacegpgmm_1_1d3d12.html#a6d077755fd1c26ad6399d41479d911bb',1,'gpgmm::d3d12']]],
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['releasememory_0',['ReleaseMemory',['../namespacegpgmm_1_1d3d12.html#a207c4b1488008518f0304784582cf52a',1,'gpgmm::d3d12']]],
+  ['releaseresourceheaps_0',['ReleaseResourceHeaps',['../namespacegpgmm_1_1d3d12.html#a06cd29b01b8c7a94760d503ca7282022',1,'gpgmm::d3d12']]],
   ['reset_1',['Reset',['../namespacegpgmm_1_1d3d12.html#a6d077755fd1c26ad6399d41479d911bb',1,'gpgmm::d3d12']]]
 ];
