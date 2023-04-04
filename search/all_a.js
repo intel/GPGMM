349922640d260feeb5390074168ebd0103c2a604
bbernhar <bbernhar@users.noreply.github.com>
2023-04-04 22:14:47 +0000

    deploy: 39d7fc4f0a7d8b800498c2352e3d3754549cd6f8

--- search/all_a.js
+++ search/all_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['querystats_0',['QueryStats',['../namespacegpgmm_1_1d3d12.html#a1912b5f127166830ad430c966ce5dfd3',1,'gpgmm::d3d12::QueryStats(RESIDENCY_MANAGER_STATS *pResidencyManagerStats)=0'],['../namespacegpgmm_1_1d3d12.html#a8c629ea6a1ef674642afb0519170c5dd',1,'gpgmm::d3d12::QueryStats(RESOURCE_ALLOCATOR_STATS *pResourceAllocatorStats)=0']]],
-  ['queryvideomemoryinfo_1',['QueryVideoMemoryInfo',['../namespacegpgmm_1_1d3d12.html#a45057cc59a73199c906220c7167cb740',1,'gpgmm::d3d12']]]
+  ['queryvideomemoryinfo_1',['QueryVideoMemoryInfo',['../namespacegpgmm_1_1d3d12.html#a347909514fa574f72b2f299cf1f54f7a',1,'gpgmm::d3d12']]]
 ];
--- search/variables_5.js
+++ search/variables_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['heapsegmentgroup_0',['HeapSegmentGroup',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html#a1d0b3786ab03261aef814ea0e0cbebee',1,'gpgmm::d3d12::HEAP_DESC']]],
+  ['heapsegment_0',['HeapSegment',['../structgpgmm_1_1d3d12_1_1_h_e_a_p___d_e_s_c.html#aa1ddee3e12372a910b3e1cd757dffb95',1,'gpgmm::d3d12::HEAP_DESC']]],
   ['heaptype_1',['HeapType',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aa720936c4215d487a3e391e144836691',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
 ];
