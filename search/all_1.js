481c709d5ce5a4cf63fa6b86594baaf7d6ee0ee9
bbernhar <bbernhar@users.noreply.github.com>
2023-04-04 21:37:15 +0000

    deploy: 88af808613f8c68c4bbdc3dd26326cd57a2ce159

--- search/all_1.js
+++ search/all_1.js
@@ -7,6 +7,6 @@ var searchData=
   ['createresidencymanager_4',['CreateResidencyManager',['../namespacegpgmm_1_1d3d12.html#adb18da09c13453d3f9024a68f43da3fd',1,'gpgmm::d3d12']]],
   ['createresource_5',['CreateResource',['../namespacegpgmm_1_1d3d12.html#a1c46f070043b15d73f162e85ad07cf60',1,'gpgmm::d3d12']]],
   ['createresourceallocator_6',['CreateResourceAllocator',['../namespacegpgmm_1_1d3d12.html#a347ae2b2328b1cb3020a3d354ff6c2d3',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, ID3D12Device *pDevice, IDXGIAdapter *pAdapter, IResourceAllocator **ppResourceAllocatorOut, IResidencyManager **ppResidencyManagerOut)'],['../namespacegpgmm_1_1d3d12.html#aee8aef833ec670d1fce4535b3627d059',1,'gpgmm::d3d12::CreateResourceAllocator(const ALLOCATOR_DESC &amp;allocatorDescriptor, ID3D12Device *pDevice, IDXGIAdapter *pAdapter, IResidencyManager *pResidencyManager, IResourceAllocator **ppResourceAllocatorOut)']]],
-  ['currentmemorycount_7',['CurrentMemoryCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#af570674ff491d0e73fd3f4b7b2527492',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]],
-  ['currentmemoryusage_8',['CurrentMemoryUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#a9f34f84b57f6c77c09ed77894cd21430',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]]
+  ['currentheapcount_7',['CurrentHeapCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#aed4a97a62d732e3ec71f4749f7e9d796',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]],
+  ['currentheapusage_8',['CurrentHeapUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#ac2cdf3d4b8f228cecb9951750da9d73c',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]]
 ];
