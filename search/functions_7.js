35be80e29e73da815aaba0baaac5d15827bc56d4
bbernhar <bbernhar@users.noreply.github.com>
2023-04-04 16:16:44 +0000

    deploy: b4b71d02a1945e3310533a21867d31e48bf54141

---
+++ search/functions_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['unlockheap_0',['UnlockHeap',['../namespacegpgmm_1_1d3d12.html#a89b7eaadd7c6e81676154b4656a0d7ad',1,'gpgmm::d3d12']]],
+  ['unmap_1',['Unmap',['../namespacegpgmm_1_1d3d12.html#a3cf28c7ae524e3045d4ef069a4d2c148',1,'gpgmm::d3d12']]]
+];
