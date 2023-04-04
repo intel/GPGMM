25d446ff6439d950178c5b06ba055ff5d1a0e5a2
bbernhar <bbernhar@users.noreply.github.com>
2023-04-04 21:28:01 +0000

    deploy: c09a79d68556416c21e26260368007ff7361bf0a

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['currentheapcount_0',['CurrentHeapCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#aed4a97a62d732e3ec71f4749f7e9d796',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]],
-  ['currentheapusage_1',['CurrentHeapUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#ac2cdf3d4b8f228cecb9951750da9d73c',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]]
+  ['currentmemorycount_0',['CurrentMemoryCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#af570674ff491d0e73fd3f4b7b2527492',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]],
+  ['currentmemoryusage_1',['CurrentMemoryUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html#a9f34f84b57f6c77c09ed77894cd21430',1,'gpgmm::d3d12::RESIDENCY_MANAGER_STATS']]]
 ];
