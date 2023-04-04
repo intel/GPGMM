feeb158e9d4cf184ee9e31593988f2571f72f657
bbernhar <bbernhar@users.noreply.github.com>
2023-04-04 18:00:43 +0000

    deploy: 1000e55f42a8a47b9ca1ed5d1b9fb546f24e8273

--- search/all_f.js
+++ search/all_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['tracefile_0',['TraceFile',['../structgpgmm_1_1d3d12_1_1_r_e_c_o_r_d___o_p_t_i_o_n_s.html#ad3157f8501a9c9ebd294b99d852eb2ae',1,'gpgmm::d3d12::RECORD_OPTIONS']]]
+  ['vulkanapiversion_0',['vulkanApiVersion',['../structgpgmm_1_1vk_1_1_gp_allocator_create_info.html#aad966715473066760a61bc34f1b8ad72',1,'gpgmm::vk::GpAllocatorCreateInfo']]]
 ];
--- search/classes_5.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['record_5foptions_0',['RECORD_OPTIONS',['../structgpgmm_1_1d3d12_1_1_r_e_c_o_r_d___o_p_t_i_o_n_s.html',1,'gpgmm::d3d12']]],
-  ['residency_5fdesc_1',['RESIDENCY_DESC',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___d_e_s_c.html',1,'gpgmm::d3d12']]],
-  ['residency_5fmanager_5fstats_2',['RESIDENCY_MANAGER_STATS',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___s_t_a_t_s.html',1,'gpgmm::d3d12']]],
-  ['resource_5fallocation_5finfo_3',['RESOURCE_ALLOCATION_INFO',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html',1,'gpgmm::d3d12']]]
-];
