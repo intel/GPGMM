5cd9c0a38e37978ccfc107324a2a48eb78dd49eb
bbernhar <bbernhar@users.noreply.github.com>
2023-04-13 22:51:29 +0000

    deploy: 13253fd7341681b4d009b41231e7194b309a8a37

--- search/variables_c.js
+++ search/variables_c.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['usedblockcount_0',['UsedBlockCount',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#a645ab3672f922f7147fa8e5c287ab129',1,'gpgmm::d3d12::ALLOCATOR_STATS']]],
-  ['usedblockusage_1',['UsedBlockUsage',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#a54c8d4605cba24a44315529194fc5f7a',1,'gpgmm::d3d12::ALLOCATOR_STATS']]],
+  ['usedblockcount_0',['UsedBlockCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#ab32aa4270acd72f44a9c434e363233b0',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_STATS']]],
+  ['usedblockusage_1',['UsedBlockUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#ad5448c6150e0a69df97f42067e595c99',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_STATS']]],
   ['usedetailedtimingevents_2',['UseDetailedTimingEvents',['../structgpgmm_1_1d3d12_1_1_r_e_c_o_r_d___o_p_t_i_o_n_s.html#a2a2bd9a23c8ad85cd7241f32ee21e66b',1,'gpgmm::d3d12::RECORD_OPTIONS']]],
-  ['usedheapcount_3',['UsedHeapCount',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#a18bf67b2f83989f01479f45d31997cc2',1,'gpgmm::d3d12::ALLOCATOR_STATS']]],
-  ['usedheapusage_4',['UsedHeapUsage',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#ad3696573187b564435547c5c1dbe4278',1,'gpgmm::d3d12::ALLOCATOR_STATS']]]
+  ['usedheapcount_3',['UsedHeapCount',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#a08db35d877456a92c75b1219bc4b8bd6',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_STATS']]],
+  ['usedheapusage_4',['UsedHeapUsage',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___s_t_a_t_s.html#ab9a75758ed2a1043e1cd7feba2e879f9',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_STATS']]]
 ];
