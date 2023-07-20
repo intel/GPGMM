673ba9f4cc293f8e3be0e13d631fc7d2606cbd38
bbernhar <bbernhar@users.noreply.github.com>
2023-07-20 01:16:41 +0000

    deploy: d31d1044a9a64fe6ff6c64ffa856675aaec8ee39

--- search/variables_6.js
+++ search/variables_6.js
@@ -9,6 +9,7 @@ var searchData=
   ['iresidencymanager_6',['IResidencyManager',['../namespacegpgmm_1_1d3d12.html#a826dccb5826564820e0671bb7b86b165',1,'gpgmm::d3d12']]],
   ['iresourceallocation_7',['IResourceAllocation',['../namespacegpgmm_1_1d3d12.html#a199143fd12adc1d2a067b51257c228f1',1,'gpgmm::d3d12']]],
   ['iresourceallocator_8',['IResourceAllocator',['../namespacegpgmm_1_1d3d12.html#adc19a7e39d7932a3c59ab30f069dc5f8',1,'gpgmm::d3d12']]],
-  ['islocked_9',['IsLocked',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___h_e_a_p___i_n_f_o.html#ac5c4aa414151e67c436afd8d620e3359',1,'gpgmm::d3d12::RESIDENCY_HEAP_INFO']]],
-  ['isresourceallocationwithincoherent_10',['IsResourceAllocationWithinCoherent',['../structgpgmm_1_1d3d12_1_1_f_e_a_t_u_r_e___d_a_t_a___r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___s_u_p_p_o_r_t.html#aac6965e75891cd2114c4ffa3837c29fe',1,'gpgmm::d3d12::FEATURE_DATA_RESOURCE_ALLOCATION_SUPPORT']]]
+  ['iscachedforresidency_9',['IsCachedForResidency',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___h_e_a_p___i_n_f_o.html#a28a9314fce5b6e9143add011f7961436',1,'gpgmm::d3d12::RESIDENCY_HEAP_INFO']]],
+  ['islocked_10',['IsLocked',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___h_e_a_p___i_n_f_o.html#ac5c4aa414151e67c436afd8d620e3359',1,'gpgmm::d3d12::RESIDENCY_HEAP_INFO']]],
+  ['isresourceallocationwithincoherent_11',['IsResourceAllocationWithinCoherent',['../structgpgmm_1_1d3d12_1_1_f_e_a_t_u_r_e___d_a_t_a___r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___s_u_p_p_o_r_t.html#aac6965e75891cd2114c4ffa3837c29fe',1,'gpgmm::d3d12::FEATURE_DATA_RESOURCE_ALLOCATION_SUPPORT']]]
 ];
