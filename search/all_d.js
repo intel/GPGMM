b47dc9fb8fb4eedbe175cc91edd0b36bd6a7ea5e
bbernhar <bbernhar@users.noreply.github.com>
2023-07-31 17:28:01 +0000

    deploy: 154215c597e5d96a352b2be542a621561c00993a

--- search/all_d.js
+++ search/all_d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['tracefile_0',['TraceFile',['../structgpgmm_1_1d3d12_1_1_r_e_c_o_r_d___o_p_t_i_o_n_s.html#ad3157f8501a9c9ebd294b99d852eb2ae',1,'gpgmm::d3d12::RECORD_OPTIONS']]]
+  ['tracefile_0',['TraceFile',['../structgpgmm_1_1d3d12_1_1_r_e_c_o_r_d___o_p_t_i_o_n_s.html#ad3157f8501a9c9ebd294b99d852eb2ae',1,'gpgmm::d3d12::RECORD_OPTIONS']]],
+  ['type_1',['Type',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___i_n_f_o.html#a2827b397bbc1f67fafefcc6ba5854df8',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_INFO']]]
 ];
