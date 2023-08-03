c76de8397eb51c42b4f435a3a540cddcb2e019f8
bbernhar <bbernhar@users.noreply.github.com>
2023-08-03 16:25:29 +0000

    deploy: edc3082cf1dc11df72f0fd97816940aed9db1bfc

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['checkfeaturesupport_0',['CheckFeatureSupport',['../namespacegpgmm_1_1d3d12.html#a07ef632845f002294d62b21ef6c36d42',1,'gpgmm::d3d12']]],
-  ['createresidencyheap_1',['CreateResidencyHeap',['../namespacegpgmm_1_1d3d12.html#aeb4ab4be463f147277b1970dcb64e1f9',1,'gpgmm::d3d12']]],
+  ['createresidencyheap_1',['CreateResidencyHeap',['../namespacegpgmm_1_1d3d12.html#aeb4ab4be463f147277b1970dcb64e1f9',1,'gpgmm::d3d12::CreateResidencyHeap(const RESIDENCY_HEAP_DESC &amp;descriptor, IResidencyManager *const pResidencyManager, CreateHeapFn createHeapFn, void *pCreateHeapContext, IResidencyHeap **ppResidencyHeapOut)'],['../namespacegpgmm_1_1d3d12.html#a7539ec907ea3866d2a3d0150fd696cf2',1,'gpgmm::d3d12::CreateResidencyHeap(const RESIDENCY_HEAP_DESC &amp;descriptor, IResidencyManager *const pResidencyManager, ID3D12Pageable *pPageable, IResidencyHeap **ppResidencyHeapOut)']]],
   ['createresidencylist_2',['CreateResidencyList',['../namespacegpgmm_1_1d3d12.html#a5f06db116d1746109ac7333dcc3411ee',1,'gpgmm::d3d12']]],
   ['createresidencymanager_3',['CreateResidencyManager',['../namespacegpgmm_1_1d3d12.html#adda8e5c707e8be72160858d53e4e8ff9',1,'gpgmm::d3d12']]],
   ['createresource_4',['CreateResource',['../namespacegpgmm_1_1d3d12.html#a1c46f070043b15d73f162e85ad07cf60',1,'gpgmm::d3d12']]],
