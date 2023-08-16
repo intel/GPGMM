3eb1dce9373cb08a152f52a7ed81adf27e4a2e54
Bernhart, Bryan <bryan.bernhart@intel.com>
2023-08-16 12:26:08 -0700

    Pre-calc buffer alignments to suppress D3D12 error.

--- src/gpgmm/d3d12/UtilsD3D12.h
+++ src/gpgmm/d3d12/UtilsD3D12.h
@@ -31,6 +31,7 @@ namespace gpgmm::d3d12 {
     const char* GetMemorySegmentName(DXGI_MEMORY_SEGMENT_GROUP heapSegment, bool isUMA);
     ID3D12Device* GetDevice(ID3D12DeviceChild* pDeviceChild);
     bool IsTexture(const D3D12_RESOURCE_DESC& resourceDescriptor);
+    bool IsBuffer(const D3D12_RESOURCE_DESC& resourceDescriptor);
 
 }  // namespace gpgmm::d3d12
 
