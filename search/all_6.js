42e5079db19002b6df6d3d3eeb598586838d811c
bbernhar <bbernhar@users.noreply.github.com>
2023-08-16 13:09:54 +0000

    deploy: 8735fe6d744c4344a68b82ed539d5208dab3e4bc

--- search/all_6.js
+++ search/all_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['heapsegment_0',['HeapSegment',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___h_e_a_p___d_e_s_c.html#a88a5c33ac93a8707bca4c6e6ac5d33f0',1,'gpgmm::d3d12::RESIDENCY_HEAP_DESC']]],
-  ['heaptype_1',['HeapType',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#afa46d69503456b012fbdde85bb7b9dad',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC']]]
+  ['heaptype_1',['HeapType',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#aa720936c4215d487a3e391e144836691',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
 ];
--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['allocation_5fflags_0',['ALLOCATION_FLAGS',['../struct_a_l_l_o_c_a_t_i_o_n___f_l_a_g_s.html',1,'']]],
-  ['allocator_5fstats_1',['ALLOCATOR_STATS',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html',1,'gpgmm::d3d12']]]
+  ['allocation_5fdesc_0',['ALLOCATION_DESC',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html',1,'gpgmm::d3d12']]],
+  ['allocation_5fflags_1',['ALLOCATION_FLAGS',['../struct_a_l_l_o_c_a_t_i_o_n___f_l_a_g_s.html',1,'']]],
+  ['allocator_5fstats_2',['ALLOCATOR_STATS',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html',1,'gpgmm::d3d12']]]
 ];
