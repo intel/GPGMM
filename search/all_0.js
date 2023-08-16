cb429ddc75195e0ec82cd9a9a08982ed4c78a206
bbernhar <bbernhar@users.noreply.github.com>
2023-08-16 13:08:35 +0000

    deploy: 0f84182a5721fc3a97b0d5f463125c7bbee560df

--- search/all_0.js
+++ search/all_0.js
@@ -13,7 +13,12 @@ var searchData=
   ['allocation_5fflag_5fnever_5fsuballocate_5fheap_10',['ALLOCATION_FLAG_NEVER_SUBALLOCATE_HEAP',['../namespacegpgmm_1_1d3d12.html#aec66b5dc9f1b55002d33c5c75ac2f122a91092b25d618e767cdfe94b5e65833f5',1,'gpgmm::d3d12']]],
   ['allocation_5fflag_5fnone_11',['ALLOCATION_FLAG_NONE',['../namespacegpgmm_1_1d3d12.html#aec66b5dc9f1b55002d33c5c75ac2f122a94853c29e79662c916a6c1b2a0ead073',1,'gpgmm::d3d12']]],
   ['allocation_5fflags_12',['ALLOCATION_FLAGS',['../struct_a_l_l_o_c_a_t_i_o_n___f_l_a_g_s.html',1,'ALLOCATION_FLAGS'],['../namespacegpgmm_1_1d3d12.html#aec66b5dc9f1b55002d33c5c75ac2f122',1,'gpgmm::d3d12::ALLOCATION_FLAGS']]],
-  ['allocator_5ffeature_13',['ALLOCATOR_FEATURE',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304a',1,'gpgmm::d3d12']]],
-  ['allocator_5ffeature_5fresource_5fallocation_5fsupport_14',['ALLOCATOR_FEATURE_RESOURCE_ALLOCATION_SUPPORT',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304aa44295c51b1df094f2bec764d5459c62b',1,'gpgmm::d3d12']]],
-  ['allocator_5fstats_15',['ALLOCATOR_STATS',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html',1,'gpgmm::d3d12']]]
+  ['allocation_5ftype_13',['ALLOCATION_TYPE',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643',1,'gpgmm::d3d12']]],
+  ['allocation_5ftype_5fstandalone_14',['ALLOCATION_TYPE_STANDALONE',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643a6bd74370eaf9acef7de02b8974d4e692',1,'gpgmm::d3d12']]],
+  ['allocation_5ftype_5fsuballocated_15',['ALLOCATION_TYPE_SUBALLOCATED',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643a07f8302922c585412449ae0528b10c41',1,'gpgmm::d3d12']]],
+  ['allocation_5ftype_5fsuballocated_5fwithin_16',['ALLOCATION_TYPE_SUBALLOCATED_WITHIN',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643a8e89dd60f649b5516bf269afc42bc424',1,'gpgmm::d3d12']]],
+  ['allocation_5ftype_5funknown_17',['ALLOCATION_TYPE_UNKNOWN',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643ada7e1adf5efc01e8f17e23b8ee88defb',1,'gpgmm::d3d12']]],
+  ['allocator_5ffeature_18',['ALLOCATOR_FEATURE',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304a',1,'gpgmm::d3d12']]],
+  ['allocator_5ffeature_5fresource_5fallocation_5fsupport_19',['ALLOCATOR_FEATURE_RESOURCE_ALLOCATION_SUPPORT',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304aa44295c51b1df094f2bec764d5459c62b',1,'gpgmm::d3d12']]],
+  ['allocator_5fstats_20',['ALLOCATOR_STATS',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_o_r___s_t_a_t_s.html',1,'gpgmm::d3d12']]]
 ];
--- search/enums_0.js
+++ search/enums_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['allocation_5fflags_0',['ALLOCATION_FLAGS',['../namespacegpgmm_1_1d3d12.html#aec66b5dc9f1b55002d33c5c75ac2f122',1,'gpgmm::d3d12']]],
-  ['allocator_5ffeature_1',['ALLOCATOR_FEATURE',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304a',1,'gpgmm::d3d12']]]
+  ['allocation_5ftype_1',['ALLOCATION_TYPE',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643',1,'gpgmm::d3d12']]],
+  ['allocator_5ffeature_2',['ALLOCATOR_FEATURE',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304a',1,'gpgmm::d3d12']]]
 ];
