eb0888b49721bf23feae5505a203c750eb09a8af
bbernhar <bbernhar@users.noreply.github.com>
2023-08-16 13:12:56 +0000

    deploy: fd3b67a461ca7e341bf8984ed0ff52afe89850c2

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -10,9 +10,5 @@ var searchData=
   ['allocation_5fflag_5fnever_5fresident_7',['ALLOCATION_FLAG_NEVER_RESIDENT',['../namespacegpgmm_1_1d3d12.html#aec66b5dc9f1b55002d33c5c75ac2f122a8e55a7791685f0adf8083d2015a7f487',1,'gpgmm::d3d12']]],
   ['allocation_5fflag_5fnever_5fsuballocate_5fheap_8',['ALLOCATION_FLAG_NEVER_SUBALLOCATE_HEAP',['../namespacegpgmm_1_1d3d12.html#aec66b5dc9f1b55002d33c5c75ac2f122a91092b25d618e767cdfe94b5e65833f5',1,'gpgmm::d3d12']]],
   ['allocation_5fflag_5fnone_9',['ALLOCATION_FLAG_NONE',['../namespacegpgmm_1_1d3d12.html#aec66b5dc9f1b55002d33c5c75ac2f122a94853c29e79662c916a6c1b2a0ead073',1,'gpgmm::d3d12']]],
-  ['allocation_5ftype_5fstandalone_10',['ALLOCATION_TYPE_STANDALONE',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643a6bd74370eaf9acef7de02b8974d4e692',1,'gpgmm::d3d12']]],
-  ['allocation_5ftype_5fsuballocated_11',['ALLOCATION_TYPE_SUBALLOCATED',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643a07f8302922c585412449ae0528b10c41',1,'gpgmm::d3d12']]],
-  ['allocation_5ftype_5fsuballocated_5fwithin_12',['ALLOCATION_TYPE_SUBALLOCATED_WITHIN',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643a8e89dd60f649b5516bf269afc42bc424',1,'gpgmm::d3d12']]],
-  ['allocation_5ftype_5funknown_13',['ALLOCATION_TYPE_UNKNOWN',['../namespacegpgmm_1_1d3d12.html#a615dbaeb4ce00f65ebb0339e48ff4643ada7e1adf5efc01e8f17e23b8ee88defb',1,'gpgmm::d3d12']]],
-  ['allocator_5ffeature_5fresource_5fallocation_5fsupport_14',['ALLOCATOR_FEATURE_RESOURCE_ALLOCATION_SUPPORT',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304aa44295c51b1df094f2bec764d5459c62b',1,'gpgmm::d3d12']]]
+  ['allocator_5ffeature_5fresource_5fallocation_5fsupport_10',['ALLOCATOR_FEATURE_RESOURCE_ALLOCATION_SUPPORT',['../namespacegpgmm_1_1d3d12.html#a24c166248417fa823597d7219566304aa44295c51b1df094f2bec764d5459c62b',1,'gpgmm::d3d12']]]
 ];
