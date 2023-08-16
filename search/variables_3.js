10d97a49b8bc06c92fc467cc08b140adb3ba27d5
bbernhar <bbernhar@users.noreply.github.com>
2023-08-16 13:29:58 +0000

    deploy: 9fd5a933884b5a551332c8942f1f81556cb65710

--- search/variables_3.js
+++ search/variables_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['eventscope_0',['EventScope',['../structgpgmm_1_1d3d12_1_1_r_e_c_o_r_d___o_p_t_i_o_n_s.html#a4e628e10277fa65899aed36a1d1fdbc8',1,'gpgmm::d3d12::RECORD_OPTIONS']]],
   ['evictsizeinbytes_1',['EvictSizeInBytes',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___d_e_s_c.html#a15ad19ff5093d252e7f51ceed22ce557',1,'gpgmm::d3d12::RESIDENCY_MANAGER_DESC']]],
-  ['extrarequiredheapflags_2',['ExtraRequiredHeapFlags',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a08cf139c6ad730e0dae3df0526ce36bd',1,'gpgmm::d3d12::ALLOCATION_DESC']]],
-  ['extrarequiredresourcepadding_3',['ExtraRequiredResourcePadding',['../structgpgmm_1_1d3d12_1_1_a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#ad125c94c6d8940bb91138434fceecf3f',1,'gpgmm::d3d12::ALLOCATION_DESC']]]
+  ['extrarequiredheapflags_2',['ExtraRequiredHeapFlags',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a133345fa1e99954f14418ed65cac9ab8',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC']]],
+  ['extrarequiredresourcepadding_3',['ExtraRequiredResourcePadding',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_i_o_n___d_e_s_c.html#a2b5fb66f8e72f4a1c285e5d84ceb3168',1,'gpgmm::d3d12::RESOURCE_ALLOCATION_DESC']]]
 ];
