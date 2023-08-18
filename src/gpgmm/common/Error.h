e387b6ab345ad1652ba0de3e749cc201933164a8
Bryan B <bryan.bernhart@intel.com>
2023-08-18 16:34:11 -0700

    Refactor TryAllocateResource error handling. (#915)

--- src/gpgmm/common/Error.h
+++ src/gpgmm/common/Error.h
@@ -90,102 +90,108 @@ namespace gpgmm {
     // Wraps a backend error code with a result object.
     // Use Result::IsSuccess then Result::AcquireResult to use or else, use Result::GetErrorCode to
     // return the error for backend-specific handling.
-    template <typename ErrorT, typename ResultT>
+    template <typename ErrorT, typename ValueT>
     class Result {
       public:
         // Empty result with error.
-        Result() : mErrorCode(ErrorCode::kUnknown) {
-            mResult = {};
+        Result() : mError(ErrorCode::kUnknown) {
+            mValue = {};
         }
 
         // Error only result
-        Result(ErrorT&& error) : mErrorCode(std::move(error)) {
-            mResult = {};
+        Result(ErrorT&& error) : mError(std::move(error)) {
+            mValue = {};
         }
 
         // Result but with no error
-        Result(ResultT&& result) : mErrorCode(ErrorCode::kNone), mResult(std::move(result)) {
+        Result(ValueT&& result) : mError(ErrorCode::kNone), mValue(std::move(result)) {
         }
 
         // Result with error.
-        Result(ErrorT&& error, ResultT&& result)
-            : mErrorCode(std::move(error)), mResult(std::move(result)) {
+        Result(ErrorT&& error, ValueT&& result)
+            : mError(std::move(error)), mValue(std::move(result)) {
         }
 
-        Result(Result<ResultT, ErrorT>&& other)
-            : mErrorCode(std::move(other.mErrorCode), mResult(std::move(other.mResult))) {
+        Result(Result<ValueT, ErrorT>&& other)
+            : mError(std::move(other.mError), mValue(std::move(other.mValue))) {
         }
 
-        Result<ResultT, ErrorT>& operator=(Result<ResultT, ErrorT>&& other) {
-            mResult = std::move(other.mResult);
-            mErrorCode = std::move(other.mErrorCode);
+        Result<ValueT, ErrorT>& operator=(Result<ValueT, ErrorT>&& other) {
+            mValue = std::move(other.mValue);
+            mError = std::move(other.mError);
             return *this;
         }
 
         ErrorCode GetErrorCode() const {
-            return mErrorCode;
+            return mError;
         }
 
-        ResultT&& AcquireResult() {
-            return std::move(mResult);
+        ValueT&& AcquireResult() {
+            return std::move(mValue);
         }
 
         ErrorT&& AcquireError() {
-            return std::move(mErrorCode);
+            return std::move(mError);
         }
 
         bool IsSuccess() const {
-            return mErrorCode == ErrorCode::kNone;
+            return mError == ErrorCode::kNone;
+        }
+
+        // Implicit conversion to MaybeError.
+        // Used by GPGMM_TRY_ASSIGN on error result.
+        operator Result<ErrorT, void>() {
+            return AcquireError();
         }
 
       private:
-        ErrorT mErrorCode;
-        ResultT mResult;
+        ErrorT mError;
+        ValueT mValue;
     };
 
-    // Specialization of Result<ErrorT, ResultT> where the ResultT is void.
+    // Specialization of Result<ErrorT, ValueT> where the ValueT is void.
     // Used when a void function must return a Result with only error code.
     template <typename ErrorT>
     class Result<ErrorT, void> {
       public:
         // Result with no error.
-        Result() : mErrorCode(ErrorCode::kNone) {
+        Result() : mError(ErrorCode::kNone) {
         }
 
         // Result with error.
-        Result(ErrorT&& error) : mErrorCode(std::move(error)) {
+        Result(ErrorT&& error) : mError(std::move(error)) {
         }
 
-        Result(Result<ErrorT, void>&& other) : mErrorCode(std::move(other.mErrorCode)) {
+        Result(Result<ErrorT, void>&& other) : mError(std::move(other.mError)) {
         }
 
         Result<ErrorT, void>& operator=(Result<ErrorT, void>&& other) {
-            mErrorCode = std::move(other.mErrorCode);
+            mError = std::move(other.mError);
             return *this;
         }
 
         ErrorCode GetErrorCode() const {
-            return mErrorCode;
+            return mError;
         }
 
         bool IsSuccess() const {
-            return mErrorCode == ErrorCode::kNone;
+            return mError == ErrorCode::kNone;
         }
 
         ErrorT&& AcquireError() {
-            return std::move(mErrorCode);
+            return std::move(mError);
         }
 
       private:
-        ErrorT mErrorCode;
+        ErrorT mError;
     };
 
     // Result with only an error code.
     using MaybeError = Result<ErrorCode, void>;
 
     // Alias of Result + error code to avoid having to always specify error type.
-    template <typename ResultT>
-    using ResultOrError = Result<ErrorCode, ResultT>;
+    template <typename ValueT>
+    using ResultOrError = Result<ErrorCode, ValueT>;
 
 }  // namespace gpgmm
 
