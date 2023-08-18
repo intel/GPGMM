1fd0f36ae1f0b8aa95e0863feba62c2c1e94e90a
bbernhar <bbernhar@users.noreply.github.com>
2023-08-18 20:32:42 +0000

    deploy: 39accf9e226cebb7ed3e6697584645620ecaaf05

--- search/variables_9.js
+++ search/variables_9.js
@@ -1,10 +1,9 @@
 var searchData=
 [
   ['recordoptions_0',['RecordOptions',['../structgpgmm_1_1d3d12_1_1_r_e_s_i_d_e_n_c_y___m_a_n_a_g_e_r___d_e_s_c.html#a2bb42b1a251c40eb5d0329a62b13f0fa',1,'gpgmm::d3d12::RESIDENCY_MANAGER_DESC::RecordOptions'],['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#aa61e37f5e895e4254cf2d3ad07f999b0',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC::RecordOptions']]],
-  ['releasesizeinbytes_1',['ReleaseSizeInBytes',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#a76b721fed3b2ce5aa24093cee1ed7c3e',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC']]],
-  ['requiredpropertyflags_2',['requiredPropertyFlags',['../structgpgmm_1_1vk_1_1_gp_resource_allocation_create_info.html#ab9451843785b4203a40d23cea9fbeee9',1,'gpgmm::vk::GpResourceAllocationCreateInfo']]],
-  ['resourcedescriptor_3',['resourceDescriptor',['../namespacegpgmm_1_1d3d12.html#a52f35a32b6cdbfb0a4055692343dece0',1,'gpgmm::d3d12']]],
-  ['resourceheapfragmentationlimit_4',['ResourceHeapFragmentationLimit',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#aeab97df27543862e4a8d935c66e8c475',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC']]],
-  ['resourceheapgrowthfactor_5',['ResourceHeapGrowthFactor',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#a17328b069c180066b9821d2efc327479',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC']]],
-  ['resourceheaptier_6',['ResourceHeapTier',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#a913a3621637a0d7cfceffab020837c26',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC']]]
+  ['requiredpropertyflags_1',['requiredPropertyFlags',['../structgpgmm_1_1vk_1_1_gp_resource_allocation_create_info.html#ab9451843785b4203a40d23cea9fbeee9',1,'gpgmm::vk::GpResourceAllocationCreateInfo']]],
+  ['resourcedescriptor_2',['resourceDescriptor',['../namespacegpgmm_1_1d3d12.html#a52f35a32b6cdbfb0a4055692343dece0',1,'gpgmm::d3d12']]],
+  ['resourceheapfragmentationlimit_3',['ResourceHeapFragmentationLimit',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#aeab97df27543862e4a8d935c66e8c475',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC']]],
+  ['resourceheapgrowthfactor_4',['ResourceHeapGrowthFactor',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#a17328b069c180066b9821d2efc327479',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC']]],
+  ['resourceheaptier_5',['ResourceHeapTier',['../structgpgmm_1_1d3d12_1_1_r_e_s_o_u_r_c_e___a_l_l_o_c_a_t_o_r___d_e_s_c.html#a913a3621637a0d7cfceffab020837c26',1,'gpgmm::d3d12::RESOURCE_ALLOCATOR_DESC']]]
 ];
