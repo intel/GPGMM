0a45f1f4c14143889a8cca602ed1302be349cb0d
bbernhar <bbernhar@users.noreply.github.com>
2023-08-25 01:15:35 +0000

    deploy: 97946833516e7e1e6f2c6e10237ff494d43eb854

--- search/functions_2.js
+++ search/functions_2.js
@@ -4,12 +4,11 @@ var searchData=
   ['getinfo_1',['GetInfo',['../namespacegpgmm_1_1d3d12.html#a260a6f663ef8bb86a09441fb159abe1e',1,'gpgmm::d3d12']]],
   ['getmemory_2',['GetMemory',['../namespacegpgmm_1_1d3d12.html#adb90c75c67c0355bca69397d8411438d',1,'gpgmm::d3d12']]],
   ['getoffsetfromresource_3',['GetOffsetFromResource',['../namespacegpgmm_1_1d3d12.html#abef42cfb572e20b44f1fc86aca14d497',1,'gpgmm::d3d12']]],
-  ['getresidencymanager_4',['GetResidencyManager',['../namespacegpgmm_1_1d3d12.html#aa899b85a6d148e56010de91a94cdde94',1,'gpgmm::d3d12']]],
-  ['getresource_5',['GetResource',['../namespacegpgmm_1_1d3d12.html#afa6f63f90f81e777ddb781a3dc01c0f2',1,'gpgmm::d3d12']]],
-  ['gpcreatebuffer_6',['gpCreateBuffer',['../namespacegpgmm_1_1vk.html#aae143de825aff02b5d4c9b316fec88b4',1,'gpgmm::vk']]],
-  ['gpcreateimage_7',['gpCreateImage',['../namespacegpgmm_1_1vk.html#a98b4ef536593f6e7d4d1fe0248c5528a',1,'gpgmm::vk']]],
-  ['gpcreateresourceallocator_8',['gpCreateResourceAllocator',['../namespacegpgmm_1_1vk.html#a714d3c0ca5f86caa30020a08646ceb65',1,'gpgmm::vk']]],
-  ['gpdestroybuffer_9',['gpDestroyBuffer',['../namespacegpgmm_1_1vk.html#a87035ca264fc0e367910c896fb73045c',1,'gpgmm::vk']]],
-  ['gpdestroyimage_10',['gpDestroyImage',['../namespacegpgmm_1_1vk.html#a8e0bd9e3e29e8f43ca886e2d7baf1ff5',1,'gpgmm::vk']]],
-  ['gpdestroyresourceallocator_11',['gpDestroyResourceAllocator',['../namespacegpgmm_1_1vk.html#acc98f4568b4de02bbd65f32c6adc7006',1,'gpgmm::vk']]]
+  ['getresource_4',['GetResource',['../namespacegpgmm_1_1d3d12.html#afa6f63f90f81e777ddb781a3dc01c0f2',1,'gpgmm::d3d12']]],
+  ['gpcreatebuffer_5',['gpCreateBuffer',['../namespacegpgmm_1_1vk.html#aae143de825aff02b5d4c9b316fec88b4',1,'gpgmm::vk']]],
+  ['gpcreateimage_6',['gpCreateImage',['../namespacegpgmm_1_1vk.html#a98b4ef536593f6e7d4d1fe0248c5528a',1,'gpgmm::vk']]],
+  ['gpcreateresourceallocator_7',['gpCreateResourceAllocator',['../namespacegpgmm_1_1vk.html#a714d3c0ca5f86caa30020a08646ceb65',1,'gpgmm::vk']]],
+  ['gpdestroybuffer_8',['gpDestroyBuffer',['../namespacegpgmm_1_1vk.html#a87035ca264fc0e367910c896fb73045c',1,'gpgmm::vk']]],
+  ['gpdestroyimage_9',['gpDestroyImage',['../namespacegpgmm_1_1vk.html#a8e0bd9e3e29e8f43ca886e2d7baf1ff5',1,'gpgmm::vk']]],
+  ['gpdestroyresourceallocator_10',['gpDestroyResourceAllocator',['../namespacegpgmm_1_1vk.html#acc98f4568b4de02bbd65f32c6adc7006',1,'gpgmm::vk']]]
 ];
